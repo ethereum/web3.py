131ad10494ab84d5eb8f00035054b3e77d3c9bd2
void <void@119.io>
2016-04-21 20:37:58 +0200

    Removed eth directory from __init__.py

--- web3/__init__.py
+++ web3/__init__.py
@@ -2,9 +2,6 @@ from __future__ import absolute_import
 
 import pkg_resources
 
-from . import (
-    eth,
-)
 from .utils.crypto import (
     sha3,
 )
@@ -13,6 +10,5 @@ __version__ = pkg_resources.get_distribution("web3.py").version
 
 __all__ = [
     "__version__",
-    "eth",
     "sha3",
 ]
