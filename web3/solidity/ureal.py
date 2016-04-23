e3f9885cddb99a623d5092519b657533368c2b0e
void <void@119.io>
2016-04-24 00:38:07 +0200

    Make isType, staticPartLength classmethods

--- web3/solidity/ureal.py
+++ web3/solidity/ureal.py
@@ -8,8 +8,10 @@ class SolidityTypeUReal(types.SolidityType):
         self._inputFormatter = f.formatInputReal
         self._outputFormatter = f.formatOutputReal
 
+    @classmethod
     def isType(self, name):
         return re.match(r"^ureal([0-9]*)?(\[([0-9]*)\])*$", name) is not None
 
+    @classmethod
     def staticPartLength(self, name):
         return 32 * self.staticArrayLength(name)
