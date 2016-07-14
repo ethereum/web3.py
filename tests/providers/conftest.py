f2396baa459c61fbbcd3c4889868f813a373d7e8
Pi Delport <pjdelport@gmail.com>
2016-07-14 17:13:57 +0200

    Add fixture for disconnected providers

---
+++ tests/providers/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+from web3.web3.ipcprovider import IPCProvider
+from web3.web3.rpcprovider import TestRPCProvider, RPCProvider
+
+
+@pytest.fixture(params=['tester', 'rpc', 'ipc'])
+def disconnected_provider(request):
+    """
+    Supply a Provider that's not connected to a node.
+
+    (See also the web3 fixture.)
+    """
+    if request.param == 'tester':
+        provider = TestRPCProvider()
+        provider.server.shutdown()
+        provider.server.server_close()
+        return provider
+    elif request.param == 'rpc':
+        return RPCProvider(port=9999)
+    elif request.param == 'ipc':
+        return IPCProvider(ipc_path='nonexistent')
+    else:
+        raise ValueError(request.param)
