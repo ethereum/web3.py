c0537f8df92c55d5a9195587c3b811997a505079
Piper Merriam <pipermerriam@gmail.com>
2016-08-03 19:54:52 -0600

    fix setgasprice test

--- tests/mining/test_miner_setGasPrice.py
+++ tests/mining/test_miner_setGasPrice.py
@@ -1,3 +1,7 @@
+import random
+import gevent
+
+
 def test_miner_setGasPrice(web3_ipc_empty, wait_for_block):
     web3 = web3_ipc_empty
 
@@ -8,7 +12,9 @@ def test_miner_setGasPrice(web3_ipc_empty, wait_for_block):
 
     web3.miner.setGasPrice(initial_gas_price // 2)
 
-    wait_for_block(web3, web3.eth.blockNumber + 2, timeout=60)
+    with gevent.Timeout(30):
+        while web3.eth.gasPrice == initial_gas_price:
+            gevent.sleep(random.random())
 
     after_gas_price = web3.eth.gasPrice
     assert after_gas_price < initial_gas_price
