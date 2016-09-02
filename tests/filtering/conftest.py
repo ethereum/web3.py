41d2447e39b76be23e096556c2bee3e1ebadbf6b
Piper Merriam <pipermerriam@gmail.com>
2016-09-02 11:51:34 -0600

    skip not implemented filter methods

--- tests/filtering/conftest.py
+++ tests/filtering/conftest.py
@@ -7,8 +7,7 @@ assert sha3_256(b'').hexdigest() == 'c5d2460186f7233c927e7db2dcc703c0e500b653ca8
 
 
 @pytest.fixture(autouse=True)
-def skip_testrpc_and_wait_for_mining_start(web3_empty, wait_for_block,
-                                           skip_if_testrpc):
+def wait_for_mining_start(web3_empty, wait_for_block):
     wait_for_block(web3_empty)
 
 
