330aa46e58023d792a40e543b38406b651d26d9d
shravan-shandilya <s.shravan95@gmail.com>
2016-09-20 15:13:19 +0530

    added delegation of shh-post

--- web3/shh.py
+++ web3/shh.py
@@ -1,3 +1,11 @@
+from web3.utils.encoding import (
+    to_decimal,
+)
+from web3.utils.functional import (
+    apply_formatters_to_return,
+)
+
+
 class Shh(object):
     """
     TODO: flesh this out.
@@ -5,11 +13,16 @@ class Shh(object):
     def __init__(self, request_manager):
         self.request_manager = request_manager
 
+    @property
+    @apply_formatters_to_return(to_decimal)
     def version(self):
         return self.request_manager.request_blocking("shh_version", [])
 
-    def post(self, *args, **kwargs):
-        raise NotImplementedError("Not Implemented")
+    def post(self, params):
+        if params and ("topic" in params) and ("payload" in params):
+            return self.request_manager.request_blocking("shh_post", [params])
+        else:
+            raise ValueError("params cannot be None or doesnot contain fields 'topic' or 'payload'")
 
     def newIdentity(self, *args, **kwargs):
         raise NotImplementedError("Not Implemented")
