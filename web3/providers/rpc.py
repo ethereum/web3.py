5dba662497b82681c6b8afa21668a55ff9dea653
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-09-22 21:46:22 +0300

    Add KeepAlive RPC provider.

--- web3/providers/rpc.py
+++ web3/providers/rpc.py
@@ -32,6 +32,12 @@ class RPCProvider(BaseProvider):
 
         super(RPCProvider, self).__init__(*args, **kwargs)
 
+    def __str__(self):
+        return "RPC connection {}:{}".format(self.host, self.port)
+
+    def __repr__(self):
+        return self.__str__()
+
     def make_request(self, method, params):
         from web3 import __version__ as web3_version
         request_data = self.encode_rpc_request(method, params)
@@ -53,6 +59,91 @@ class RPCProvider(BaseProvider):
         with contextlib.closing(client):
             response = client.post(self.path, body=request_data)
             response_body = response.read()
+
+        return response_body
+
+
+class KeepAliveRPCProvider(BaseProvider):
+    """RPC-provider that handles HTTP keep-alive connection correctly.
+
+    HTTP client is recycled across requests. Create only one instance of KeepAliveProvider per process.
+    """
+
+    #: In-process client cache keyed by host:port -> HTTPClient
+    clients = {}
+
+    def __init__(self,
+                 host="127.0.0.1",
+                 port="8545",
+                 path="/",
+                 ssl=False,
+                 connection_timeout=10,
+                 network_timeout=10,
+                 concurrency=10,
+                 *args,
+                 **kwargs):
+        """Create a new RPC client.
+
+        :param concurrency: See :class:`geventhttpclient.HTTPClient`
+
+        :param connection_timeout: See :class:`geventhttpclient.HTTPClient`
+
+        :param network_timeout: See :class:`geventhttpclient.HTTPClient`
+        """
+        self.host = host
+        self.port = int(port)
+        self.path = path
+        self.ssl = ssl
+        self.connection_timeout = connection_timeout
+        self.network_timeout = network_timeout
+        self.concurrency = self.concurrency
+
+        super(KeepAliveRPCProvider, self).__init__(*args, **kwargs)
+
+        self.client = self.get_or_create_client()
+
+    def get_or_create_client(self):
+        from web3 import __version__ as web3_version
+
+        key = "{}:{}".format(self.host, self.port)
+        client = KeepAliveRPCProvider.clients.get(key)
+        if client:
+            # Get in-process client instance for this host
+            return client
+
+        request_user_agent = 'Web3.py/{version}/{class_name}'.format(
+            version=web3_version,
+            class_name=type(self),
+        )
+
+        client = HTTPClient(
+            host=self.host,
+            port=self.port,
+            ssl=self.ssl,
+            connection_timeout=self.connection_timeout,
+            network_timeout=self.network_timeout,
+            concurrency=self.concurrency,
+            headers={
+                'Content-Type': 'application/json',
+                'User-Agent': request_user_agent,
+            },
+        )
+
+        self.clients[key] = client
+        return client
+
+    def __str__(self):
+        return "Keep-alive RPC connection {}:{}".format(self.host, self.port)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def make_request(self, method, params):
+
+        request_data = self.encode_rpc_request(method, params)
+
+        response = self.client.post(self.path, body=request_data)
+        response_body = response.read()
 
         return response_body
 
@@ -90,3 +181,4 @@ class TestRPCProvider(RPCProvider):
         self.thread = gevent.spawn(self.server.serve_forever)
 
         super(TestRPCProvider, self).__init__(host, str(port), *args, **kwargs)
+
