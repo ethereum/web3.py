258714a5dd691b7c61256752af98b15998bfafda
Agnieszka <agnieszka.kramarek@imapp.pl>
2016-10-12 09:50:20 +0200

    fixed tests

--- tests/contracts/test_contract_transact_interface.py
+++ tests/contracts/test_contract_transact_interface.py
@@ -64,6 +64,7 @@ def test_transacting_with_contract_with_arguments(web3_tester,
 
 
 def test_deploy_when_default_account_is_different_than_coinbase(web3_tester,
+                                                                wait_for_transaction,
                                                                 STRING_CONTRACT):
     web3_tester.eth.defaultAccount = web3_tester.eth.accounts[1]
     assert web3_tester.eth.defaultAccount != web3_tester.eth.coinbase
@@ -71,20 +72,21 @@ def test_deploy_when_default_account_is_different_than_coinbase(web3_tester,
     StringContract = web3_tester.eth.contract(**STRING_CONTRACT)
 
     deploy_txn = StringContract.deploy(args=["Caqalai"])
-    deploy_receipt = wait_for_transaction_receipt(web3_tester, deploy_txn, 30)
-    assert deploy_receipt['from'] == web3_tester.eth.defaultAccount
+    wait_for_transaction(web3_tester, deploy_txn)
+    txn_after = web3_tester.eth.getTransaction(deploy_txn)
+    assert txn_after['from'] == web3_tester.eth.defaultAccount
 
 
 def test_transact_when_default_account_is_different_than_coinbase(web3_tester,
-                                                                  math_contract,
-                                                                  transact_args,
-                                                                  transact_kwargs):
+                                                                  wait_for_transaction,
+                                                                  math_contract):
     web3_tester.eth.defaultAccount = web3_tester.eth.accounts[1]
     assert web3_tester.eth.defaultAccount != web3_tester.eth.coinbase
 
-    txn_hash = math_contract.transact().increment(*transact_args, **transact_kwargs)
-    txn_receipt = web3_tester.eth.getTransactionReceipt(txn_hash)
-    assert txn_receipt['from'] == web3_tester.eth.defaultAccount
+    txn_hash = math_contract.transact().increment()
+    wait_for_transaction(web3_tester, txn_hash)
+    txn_after = web3_tester.eth.getTransaction(txn_hash)
+    assert txn_after['from'] == web3_tester.eth.defaultAccount
 
 
 def test_transacting_with_contract_with_string_argument(web3_tester, string_contract):
