778a84d42e69f0148413b2f95a3bf4c9c11071a0
shravan-shandilya <s.shravan95@gmail.com>
2016-10-24 10:42:39 +0530

    fix for issue: 106

--- tests/filtering/test_filter_against_latest_blocks.py
+++ tests/filtering/test_filter_against_latest_blocks.py
@@ -16,9 +16,8 @@ def test_filter_against_latest_blocks(web3_empty, wait_for_block, skip_if_testrp
 
     wait_for_block(web3, current_block + 3)
 
-    with gevent.Timeout(5):
-        while len(seen_blocks) < 2:
-            gevent.sleep(random.random())
+    while len(seen_blocks) < 2:
+        pass
 
     txn_filter.stop_watching(3)
 
