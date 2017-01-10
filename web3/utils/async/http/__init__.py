97971cd41d9d340a932ec8e499ed2d8c3476e80e
Piper Merriam <pipermerriam@gmail.com>
2017-01-10 09:06:20 -0700

    fix imports

--- web3/utils/async/http/__init__.py
+++ web3/utils/async/http/__init__.py
@@ -2,10 +2,10 @@ import os
 
 
 if 'WEB3_ASYNC_GEVENT' in os.environ:
-    from .requests_http import (
+    from .gevent_http import (  # noqa: F401
         make_post_request,
     )
 else:
-    from .gevent_http import (  # noqa: F401
+    from .requests_http import (  # noqa: F401
         make_post_request,
     )
