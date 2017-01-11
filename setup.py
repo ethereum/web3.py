24bacea2a672f1485dad2441c70cc0fc6cc20e74
Piper Merriam <pipermerriam@gmail.com>
2017-01-10 22:27:16 -0700

    3.5.0 Release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ readme = open(os.path.join(DIR, 'README.md')).read()
 
 setup(
     name='web3',
-    version='3.4.4',
+    version='3.5.0',
     description="""Web3.py""",
     long_description=readme,
     author='Piper Merriam',
