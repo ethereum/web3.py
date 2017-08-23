2dc63af7ebceeac10a2fd979a73ebad195dffad0
Jason Carver <ut96caarrs@snkmail.com>
2017-08-23 14:09:27 -0700

    New method to recursively create AttributeDict
    Plus a few helpers to map a function to a collection and return the same
    type.
    
    Largely punts on cycle references, by raising Exception with nice
    message.

--- tests/utilities/test_attributedict.py
+++ tests/utilities/test_attributedict.py
@@ -74,3 +74,34 @@ def test_attributedict_inequality(dict1, dict2):
     assert AttributeDict(dict1) != dict2
     assert AttributeDict(dict1) != AttributeDict(dict2)
     assert dict1 != AttributeDict(dict2)
+
+
+def test_attributedict_recursive_dict():
+    w = AttributeDict.recursive({'x': {'y': {'z': 8}}})
+    assert w.x.y.z == 8
+
+
+@pytest.mark.parametrize('sequence', [list, tuple])
+def test_attributedict_sequence_with_dict(sequence):
+    data = sequence(['a', {'found': True}, 'c'])
+    dict_in_sequence = AttributeDict.recursive(data)
+    assert dict_in_sequence[1].found is True
+
+
+def test_attributedict_dict_in_list_in_dict():
+    data = {'instructions': [
+        0,
+        1,
+        'neither shalt thou count, excepting that thou then proceedeth to three',
+        {'if_naughty': 'snuff it'},
+        'shalt thou not count',
+        'right out',
+    ]}
+    attrdict = AttributeDict.recursive(data)
+    assert attrdict.instructions[3].if_naughty == 'snuff it'
+
+
+def test_attributedict_set_in_recursive_dict():
+    data = {'mydict': {'myset': {'found'}}}
+    attrdict = AttributeDict.recursive(data)
+    assert 'found' in attrdict.mydict.myset
--- web3/utils/decorators.py
+++ web3/utils/decorators.py
@@ -13,3 +13,20 @@ class combomethod(object):
             else:
                 return self.method(objtype, *args, **kwargs)
         return _wrapper
+
+
+def reject_recursive_repeats(to_wrap):
+    '''
+    Prevent simple cycles by returning None when called recursively with same instance
+    '''
+    to_wrap.__already_called = {}
+
+    def wrapped(*args):
+        instances = tuple(map(id, args))
+        if instances in to_wrap.__already_called:
+            return None
+        to_wrap.__already_called[instances] = True
+        wrapped_val = to_wrap(*args)
+        del to_wrap.__already_called[instances]
+        return wrapped_val
+    return wrapped
