580f2470bf14882ae799c4a44997d95291d6f996
Piper Merriam <pipermerriam@gmail.com>
2017-08-26 00:04:35 -0600

    stand-alone: remove unimplemented net apis, add version

--- web3/net.py
+++ web3/net.py
@@ -8,12 +8,10 @@ class Net(Module):
     def listening(self):
         return self.web3.manager.request_blocking("net_listening", [])
 
-    def getListening(self, *args, **kwargs):
-        raise NotImplementedError("Async calling has not been implemented")
-
     @property
     def peerCount(self):
         return self.web3.manager.request_blocking("net_peerCount", [])
 
-    def getPeerCount(self, *args, **kwargs):
-        raise NotImplementedError("Async calling has not been implemented")
+    @property
+    def version(self):
+        return self.web3.manager.request_blocking("net_version", [])
