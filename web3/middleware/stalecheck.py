4846429e803451810692106b3121cb658e7ef708
Piper Merriam <pipermerriam@gmail.com>
2017-09-08 11:51:02 -0600

    standalone: unicode_literals

--- web3/middleware/stalecheck.py
+++ web3/middleware/stalecheck.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 import time
 
 from web3.exceptions import StaleBlockchain
--- web3/utils/blocks.py
+++ web3/utils/blocks.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from eth_utils import (
     is_hex,
     is_string,
