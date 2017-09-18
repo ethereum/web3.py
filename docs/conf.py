ff4fcd10aadef2f49954abadd0ab660f8983c7ed
Jason Carver <ut96caarrs@snkmail.com>
2017-09-18 10:53:52 -0700

    docs: style configuration cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'better'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -104,8 +104,6 @@ html_theme = 'better'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-from better import better_theme_path
-html_theme_path = [better_theme_path]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -255,9 +253,3 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
-
-import sphinx_rtd_theme
-
-html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
