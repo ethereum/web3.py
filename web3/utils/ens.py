872832a39536bab30dff12a26ac2c35853181a38
Jason Carver <ut96caarrs@snkmail.com>
2017-10-12 19:31:40 -0700

    ens: disallow ENS names encoded to bytes
    `bytes` there should be treated as an address. Otherwise there is an
    ambiguity with addresses passed in as bytes that happen to end in
    0x2e657468

--- web3/utils/ens.py
+++ web3/utils/ens.py
@@ -5,8 +5,6 @@ from cytoolz import (
 
 
 def is_ens_name(value):
-    if isinstance(value, bytes):
-        value = value.decode('utf-8')
     if not isinstance(value, str):
         return False
     return value.endswith('.eth')
