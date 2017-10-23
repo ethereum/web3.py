737dfe13ba56e10b64e6679e8f86ff9d3ea2335f
Jason Carver <ut96caarrs@snkmail.com>
2017-10-23 14:53:29 -0700

    sha3: remove encoding param, old primitive logic

--- tests/core/web3-module/test_sha3.py
+++ tests/core/web3-module/test_sha3.py
@@ -1,9 +1,6 @@
 # coding=utf-8
 
-from __future__ import unicode_literals
-
 import pytest
-import sys
 
 from web3 import Web3
 
@@ -31,6 +28,19 @@ def test_sha3_hexstr(hexstr, digest):
 
 
 @pytest.mark.parametrize(
+    'primitive, exception',
+    [
+        ('0x0', TypeError),
+        ('', TypeError),
+        (-1, ValueError),
+    ]
+)
+def test_sha3_primitive_invalid(primitive, exception):
+    with pytest.raises(exception):
+        Web3.sha3(primitive)
+
+
+@pytest.mark.parametrize(
     'primitive, digest',
     [
         (b'cowm\xc3\xb6', '0x0f355f04c0a06eebac1d219b34c598f85a1169badee164be8a30345944885fe8'),
@@ -38,12 +48,7 @@ def test_sha3_hexstr(hexstr, digest):
     ]
 )
 def test_sha3_primitive(primitive, digest):
-    if primitive and sys.version_info[0] < 3:
-        # py2 doesn't recognize bytes, tries to decode as hexstr.
-        # Old implementation would have failed, too
-        pytest.raises(TypeError)
-    else:
-        assert Web3.sha3(primitive) == digest
+    assert Web3.sha3(primitive) == digest
 
 
 @pytest.mark.parametrize(
