6791b284b92393498c7d032313a80331587a1e33
Mo <mnokhb@gmail.com>
2017-11-19 12:43:50 +0000

    Tests flake8

--- tests/core/contracts/test_contract_prepareTransaction.py
+++ tests/core/contracts/test_contract_prepareTransaction.py
@@ -2,17 +2,6 @@
 
 import pytest
 
-from eth_utils import (
-    force_bytes,
-)
-
-from web3.utils.empty import (
-    empty,
-)
-from web3.utils.transactions import (
-    wait_for_transaction_receipt,
-)
-
 # Ignore warning in pyethereum 1.6 - will go away with the upgrade
 pytestmark = pytest.mark.filterwarnings("ignore:implicit cast from 'char *'")
 
@@ -36,11 +25,17 @@ def test_prepare_transacting_with_contract_no_arguments(web3, math_contract):
 @pytest.mark.parametrize(
     'transaction_args,method_args,method_kwargs,expected',
     (
-        ({}, (5,), {}, {'data': '0x7cf5dab00000000000000000000000000000000000000000000000000000000000000005'}),
+        ({},
+         (5,),
+         {},
+         {'data': '0x7cf5dab00000000000000000000000000000000000000000000000000000000000000005'}),
     ),
 )
-def test_prepare_transacting_with_contract_with_arguments(web3, math_contract, transaction_args, method_args, method_kwargs, expected):
-    txn = math_contract.prepareTransaction(transaction_args).increment(*method_args, **method_kwargs)
+def test_prepare_transacting_with_contract_with_arguments(web3, math_contract, transaction_args,
+                                                          method_args, method_kwargs, expected):
+    txn = math_contract.prepareTransaction(transaction_args).increment(
+        *method_args, **method_kwargs
+    )
     expected['to'] = math_contract.address
     assert txn is not None
     assert txn == expected
