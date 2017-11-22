821f45d85b7bfe66382b3d21c178fe22ad7cf97d
Jason Carver <ut96caarrs@snkmail.com>
2017-11-22 08:32:43 -0800

    Bump version: 4.0.0-beta.1 → 4.0.0-beta.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ if sys.platform == 'win32':
 setup(
     name='web3',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='4.0.0-beta.1',
+    version='4.0.0-beta.2',
     description="""Web3.py""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
