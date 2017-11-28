495a4780c68631e119dcdec06404095e52f532fc
Jason Carver <ut96caarrs@snkmail.com>
2017-11-28 15:28:23 -0800

    demonstrate IPC crash with no path

---
+++ tests/core/providers/test_ipc_provider.py
@@ -0,0 +1,12 @@
+from web3 import Web3
+from web3.providers.ipc import (
+    IPCProvider,
+)
+
+
+def test_ipc_no_path():
+    """
+    IPCProvider.isConnected() returns False when no path is supplied
+    """
+    ipc = IPCProvider(None)
+    assert ipc.isConnected() is False
