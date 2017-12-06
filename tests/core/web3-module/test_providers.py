ecc79720494e37a375a4d7fa69c9163527119c27
Jason Carver <ut96caarrs@snkmail.com>
2017-12-06 10:54:56 -0800

    basic auto-provider test with no connections

--- tests/core/web3-module/test_providers.py
+++ tests/core/web3-module/test_providers.py
@@ -1,3 +1,7 @@
+import pytest
+
+from web3 import Web3
+from web3.exceptions import UnhandledRequest
 from web3.providers.eth_tester import EthereumTesterProvider
 
 
@@ -15,3 +19,15 @@ def test_set_providers_single(web3):
     web3.providers = providers[0]
 
     assert web3.providers == providers
+
+
+def test_auto_provider_none():
+    # init without provider succeeds, even when no provider available
+    w3 = Web3()
+
+    # non-node requests succeed
+    w3.toHex(0) == '0x0'
+
+    # node requests fail
+    with pytest.raises(UnhandledRequest):
+        w3.eth.coinbase
