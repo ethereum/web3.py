945ad07d1df4aabb802651fa6f529d502973d8cd
Jason Haas <jasonrhaas@gmail.com>
2017-12-20 17:27:20 -0600

    Still need to work around fixture() issue
    Going to make the accounts and transactions as
    pytest.mark.paramatrize() instead.

--- tests/core/middleware/test_signing_middleware.py
+++ tests/core/middleware/test_signing_middleware.py
@@ -5,7 +5,7 @@ from eth_utils import (
     is_dict,
 )
 import web3
-from web3 import Web3, EthereumTesterProvider
+from web3 import Web3, EthereumTesterProvider, TestRPCProvider
 # from web3.utils.encoding import to_bytes, to_hex
 from web3.middleware import (
     construct_transaction_signing_middleware,
@@ -18,7 +18,7 @@ hex_str_private_key = '0xd65215905d39210e8c1aba8449d0ed3a95f2bd4b8d5f1f253135986
 @pytest.fixture(scope='module')
 def w3():
     # Might want to use EthereumTesterProvider instead
-    instance = Web3(EthereumTesterProvider())
+    instance = Web3(TestRPCProvider())
     return instance
 
 
@@ -33,17 +33,6 @@ def testrpc_account(request, w3):
 
 
 @pytest.fixture(params=[bytes_private_key], ids=['bytes_pk_1'])
-def eth_key_account(request):
-    eth_key_acct = keys.PrivateKey(bytes_private_key)
-    ek = dict(private_key=eth_key_acct,
-              public_key=eth_key_acct.public_key,
-              address=eth_key_acct.public_key.to_address()
-              )
-
-    return ek
-
-
-@pytest.fixture(params=[bytes_private_key], ids=['bytes_pk_1'])
 def web3_account(request, w3):
     web3_acct = w3.eth.account.create(request.param)
     w3a = dict(private_key=web3_acct.privateKey,
@@ -54,16 +43,27 @@ def web3_account(request, w3):
     return w3a
 
 
-@pytest.fixture()
-def eth_key_account_transaction(eth_key_account, w3):
-    tx = {'from': eth_key_account['address'], 'to': w3.eth.accounts[0], 'value': 123}
+@pytest.fixture(params=[bytes_private_key], ids=['bytes_pk_1'])
+def eth_key_account(request):
+    eth_key_acct = keys.PrivateKey(request.param)
+    ek = dict(private_key=eth_key_acct.to_hex(),
+              public_key=eth_key_acct.public_key,
+              address=eth_key_acct.public_key.to_address()
+              )
 
-    return tx
+    return ek
 
 
 @pytest.fixture()
 def web3_account_transaction(web3_acccount, w3):
     tx = {'from': web3_acccount['address'], 'to': w3.eth.accounts[0], 'value': 123}
+
+    return tx
+
+
+@pytest.fixture()
+def eth_key_account_transaction(eth_key_account, w3):
+    tx = {'from': eth_key_account['address'], 'to': w3.eth.accounts[0], 'value': 123}
 
     return tx
 
@@ -82,6 +82,7 @@ def _make_request(method, params):
 
 # I wish fixtures could be passed as params :(
 # https://github.com/pytest-dev/pytest/issues/349
+@pytest.mark.skip
 @pytest.mark.parametrize(
     'method,params,expected',
     (
@@ -89,14 +90,14 @@ def _make_request(method, params):
         ('eth_sendTransaction', [], 'transaction'),
     )
 )
-def test_transaction_signing_middleware(method, params, expected, w3, private_key, testrpc_account, web3_account, eth_key_account):
+def test_transaction_signing_middleware(method, params, expected, w3, testrpc_account, web3_account, eth_key_account):
     local_to_web3_tx = {'from': web3_account['address'], 'to': testrpc_account['address'], 'value': 123}
     local_to_eth_key_tx = {'from': eth_key_account['address'], 'to': testrpc_account['address'], 'value': 123}
 
     middleware = construct_transaction_signing_middleware(private_key)(_make_request, w3)
     # import pdb; pdb.set_trace()
 
-    # actual = middleware(method, params)
+    actual = middleware(method=method, params=local_to_web3_tx)
     # assert is_dict(actual)
     # assert 'result' in actual
     # assert actual['result'] == expected
