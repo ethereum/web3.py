f4aceac0c057a931204032225eaed2b27b5986bc
Jason Haas <jasonrhaas@gmail.com>
2017-12-20 17:25:16 -0600

    Work around `nonce` issue

--- web3/middleware/signing.py
+++ web3/middleware/signing.py
@@ -32,10 +32,12 @@ def construct_transaction_signing_middleware(private_key):
     # - bytes
     # TODO: - hexbytes
     valid_key_types = (PrivateKey, HexBytes, bytes)
-    _private_key = None
+
+    _private_key = private_key if valid_key_types else None
 
     if isinstance(private_key, valid_key_types):
         # Might also want to check that bytes length == 32
+        # eth_utils.is_32byte_address
         _private_key = private_key
 
     def transaction_signing_middleware(make_request, web3):
@@ -46,22 +48,23 @@ def construct_transaction_signing_middleware(private_key):
             if method != 'eth_sendTransaction':
                 return make_request(method, params)
 
-            transaction = params[0]
+            transaction = params
             transaction_from_address = transaction.get('from')
             private_key_address = web3.eth.account.privateKeyToAccount(_private_key).address
 
             if is_same_address(transaction_from_address, private_key_address):
                 return make_request(method, params)
 
+            if 'gas' not in transaction:
+                # import pdb; pdb.set_trace()
+                transaction['gas'] = web3.eth.estimateGas(transaction)
+            if 'gas_price' not in transaction:
+                transaction['gasPrice'] = web3.eth.gasPrice
+            if 'nonce' not in transaction:
+                transaction['nonce'] = web3.eth.getTransactionCount(transaction_from_address)
+
             signed = web3.eth.account.signTransaction(transaction, _private_key)
             raw_transaction = signed.rawTransaction
-
-            if 'nonce' not in raw_transaction:
-                raw_transaction['nonce'] = web3.eth.getTransactionCount(transaction_from_address)
-            if 'gas' not in raw_transaction:
-                raw_transaction['gas'] = web3.eth.estimateGas(transaction)
-            if 'gas_price' not in raw_transaction:
-                raw_transaction['gasPrice'] = web3.eth.gasPrice
 
             return make_request(method='eth_sendRawTransaction', params=[raw_transaction])
         return middleware
