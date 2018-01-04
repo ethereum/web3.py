cae705f39e296f1156892301104be6e6243718d0
bweick <brian@biometrixtech.com>
2018-01-04 14:40:53 -0700

    Change check_method tests to directly test function.

--- tests/core/middleware/test_http_request_retry.py
+++ tests/core/middleware/test_http_request_retry.py
@@ -2,11 +2,12 @@ import pytest
 from unittest.mock import Mock
 
 from web3.middleware import http_retry_request_middleware
+from web3.middleware.http_retry_request import check_method
 
 
 @pytest.fixture
 def http_retry_request_setup():
-    make_request, web3, check_method = Mock(), Mock(), Mock()
+    make_request, web3 = Mock(), Mock()
     setup = http_retry_request_middleware(make_request, web3)
     setup.web3 = web3
     setup.make_request = make_request
@@ -14,13 +15,13 @@ def http_retry_request_setup():
     return setup
 
 
-# def test_check_method_false(http_retry_request_setup):
-#     method = 'eth_sendTransaction'
-#     params = None
-#
-#     http_retry_request_setup(method, params)
-#     http_retry_request_setup.check_method.assert_called_once_with(method)
-#     assert http_retry_request_setup.check_method == False
+def test_check_method_false(http_retry_request_setup):
+    methods = ['eth_sendTransaction', 'eth_sendRawTransaction',
+    'personal_signAndSendTransaction', 'personal_sendTRansaction']
+
+    for method in methods:
+        check_method.assert_called_once_with(method)
+        assert check_method(method) == False
 
 
 def test_check_send_transaction_called_once(http_retry_request_setup):
