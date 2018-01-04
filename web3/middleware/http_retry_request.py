5dbd49b88cd28d2767331d4afe57e76b7874f3ef
bweick <brian@biometrixtech.com>
2018-01-04 15:00:44 -0700

    Moving whitelist outside of main function.

--- web3/middleware/http_retry_request.py
+++ web3/middleware/http_retry_request.py
@@ -1,9 +1,55 @@
 from requests.exceptions import HTTPError, ConnectionError, Timeout, TooManyRedirects
 
-'''
-Creates middleware that retries failed HTTP requests. Is a default
-middleware for HTTPProvider.
-'''
+whitelist = [
+    'admin',
+    'shh',
+    'miner',
+    'net',
+    'txpool'
+    'testing',
+    'evm',
+    'eth_protocolVersion',
+    'eth_syncing',
+    'eth_coinbase',
+    'eth_mining',
+    'eth_hashrate',
+    'eth_gasPrice',
+    'eth_accounts',
+    'eth_blockNumber',
+    'eth_getBalance',
+    'eth_getStorageAt',
+    'eth_getCode',
+    'eth_getBlockByNumber',
+    'eth_getBlockByHash',
+    'eth_getBlockTransactionCountByNumber',
+    'eth_getBlockTransactionCountByHash',
+    'eth_getUncleCountByBlockNumber',
+    'eth_getUncleCountByBlockHash',
+    'eth_getTransactionByHash',
+    'eth_getTransactionByBlockHashAndIndex',
+    'eth_getTransactionByBlockNumberAndIndex',
+    'eth_getTransactionReceipt',
+    'eth_getTransactionCount',
+    'eth_call',
+    'eth_estimateGas',
+    'eth_newBlockFilter',
+    'eth_newPendingTransactionFilter',
+    'eth_newFilter',
+    'eth_getFilterChanges',
+    'eth_getFilterLogs',
+    'eth_getLogs',
+    'eth_uninstallFilter',
+    'eth_getCompilers',
+    'eth_getWork',
+    'eth_sign',
+    'personal_importRawKey',
+    'personal_newAccount',
+    'personal_listAccounts',
+    'personal_lockAccount',
+    'personal_unlockAccount',
+    'personal_ecRecover',
+    'personal_sign'
+]
 
 
 def check_method(method):
@@ -17,59 +63,10 @@ def check_method(method):
 
 
 def http_retry_request_middleware(make_request, web3):
-
-    whitelist = [
-        'admin',
-        'shh',
-        'miner',
-        'net',
-        'txpool'
-        'testing',
-        'evm',
-        'eth_protocolVersion',
-        'eth_syncing',
-        'eth_coinbase',
-        'eth_mining',
-        'eth_hashrate',
-        'eth_gasPrice',
-        'eth_accounts',
-        'eth_blockNumber',
-        'eth_getBalance',
-        'eth_getStorageAt',
-        'eth_getCode',
-        'eth_getBlockByNumber',
-        'eth_getBlockByHash',
-        'eth_getBlockTransactionCountByNumber',
-        'eth_getBlockTransactionCountByHash',
-        'eth_getUncleCountByBlockNumber',
-        'eth_getUncleCountByBlockHash',
-        'eth_getTransactionByHash',
-        'eth_getTransactionByBlockHashAndIndex',
-        'eth_getTransactionByBlockNumberAndIndex',
-        'eth_getTransactionReceipt',
-        'eth_getTransactionCount',
-        'eth_call',
-        'eth_estimateGas',
-        'eth_newBlockFilter',
-        'eth_newPendingTransactionFilter',
-        'eth_newFilter',
-        'eth_getFilterChanges',
-        'eth_getFilterLogs',
-        'eth_getLogs',
-        'eth_uninstallFilter',
-        'eth_getCompilers',
-        'eth_getWork',
-        'eth_sign',
-        'personal_importRawKey',
-        'personal_newAccount',
-        'personal_listAccounts',
-        'personal_lockAccount',
-        'personal_unlockAccount',
-        'personal_ecRecover',
-        'personal_sign'
-    ]
-
-
+    '''
+    Creates middleware that retries failed HTTP requests. Is a default
+    middleware for HTTPProvider.
+    '''
     def middleware(method, params):
         if check_method(method):
             MAX_TRIES = 5
