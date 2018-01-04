cb7c8edee04e1e20f3be3995675248b656c9caaf
bweick <brian@biometrixtech.com>
2018-01-04 13:04:26 -0700

    Adding first couple tests to handle eth_sendTransaction methods.

---
+++ tests/core/middleware/test_http_request_retry.py
@@ -0,0 +1,31 @@
+import pytest
+from unittest.mock import Mock
+
+from web3.middleware import http_retry_request_middleware
+
+
+@pytest.fixture
+def http_retry_request_setup():
+    make_request, web3 = Mock(), Mock()
+    setup = http_retry_request_middleware(make_request, web3)
+    setup.web3 = web3
+    setup.make_request = make_request
+    return setup
+
+def test_check_method_false(http_retry_request_setup):
+    method = 'eth_sendTransaction'
+    params = None
+
+    http_retry_request_setup(method, params)
+    http_retry_request_setup.check_method.assert_called_once_with(method)
+    assert http_retry_request_setup.check_method == False
+
+def test_check_send_transaction_called_once(http_retry_request_setup):
+    method = 'eth_sendTransaction'
+    params = [{
+        'to': '0x0',
+        'value': 1,
+    }]
+
+    http_retry_request_setup(method, params)
+    http_retry_request_setup.make_request.assert_called_once_with(method, params)
