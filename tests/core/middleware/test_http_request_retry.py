1db5b1524944f286ad5b2ce1d22386b4a8f60a74
bweick <brian@biometrixtech.com>
2018-01-05 11:39:32 -0700

    Added check_method true test. Edited transaction method test so truly tests if called only once vs. complement. Handled error in test_valid_method. Added test to check if retry middleware is a default middleware for strictly rpc providers.

--- tests/core/middleware/test_http_request_retry.py
+++ tests/core/middleware/test_http_request_retry.py
@@ -3,6 +3,8 @@ from unittest.mock import Mock
 
 from web3.middleware import http_retry_request_middleware
 from web3.middleware.http_retry_request import check_method
+from requests.exceptions import ConnectionError
+from web3.providers import HTTPProvider, IPCProvider
 
 
 @pytest.fixture
@@ -16,11 +18,15 @@ def http_retry_request_setup():
 
 
 def test_check_method_false(http_retry_request_setup):
-    methods = ['eth_sendTransaction', 'eth_sendRawTransaction',
-                'personal_signAndSendTransaction', 'personal_sendTRansaction']
+    methods = ['eth_sendTransaction', 'personal_signAndSendTransaction', 'personal_sendTRansaction']
 
     for method in methods:
         assert check_method(method) == False
+
+
+def test_check_method_true(http_retry_request_setup):
+    method = 'eth_getBalance'
+    assert check_method(method) == True
 
 
 def test_check_send_transaction_called_once(http_retry_request_setup):
@@ -30,14 +36,25 @@ def test_check_send_transaction_called_once(http_retry_request_setup):
         'value': 1,
     }]
 
-    http_retry_request_setup(method, params)
-    http_retry_request_setup.make_request.assert_called_once_with(method, params)
+    http_retry_request_setup.make_request.side_effect = ConnectionError
+    with pytest.raises(ConnectionError):
+        http_retry_request_setup(method, params)
+    assert http_retry_request_setup.make_request.call_count == 1
 
 
-def test_valid_mathod_retried(http_retry_request_setup):
+def test_valid_method_retried(http_retry_request_setup):
     method = 'eth_getBalance'
     params = []
-    http_retry_request_setup.make_request.return_value = ConnectionError
+    http_retry_request_setup.make_request.side_effect = ConnectionError
 
-    http_retry_request_setup(method, params)
+    with pytest.raises(ConnectionError):
+        http_retry_request_setup(method, params)
     assert http_retry_request_setup.make_request.call_count == 5
+
+
+def test_is_strictly_default_http_middleware():
+    web3 = HTTPProvider()
+    assert 'http_retry_request' in web3.middlewares
+
+    web3 = IPCProvider()
+    assert 'http_retry_request' not in web3.middlewares
