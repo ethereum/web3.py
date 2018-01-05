76fe8c8c9ff1873cd1b0e75b4670fa3dde1570e7
Piper Merriam <pipermerriam@gmail.com>
2018-01-05 16:06:52 -0700

    Add gas-station style gas pricing

---
+++ tests/core/gas-strategies/test_time_based_gas_price_strategy.py
@@ -0,0 +1,132 @@
+import pytest
+
+from web3 import Web3
+from web3.providers.base import BaseProvider
+from web3.gas_strategies.time_based import (
+    construct_time_based_gas_price_strategy,
+)
+from web3.middleware import (
+    construct_fixture_middleware,
+)
+
+
+def wrap_result(fn):
+    def inner(*args, **kwargs):
+        return {
+            'result': fn(*args, **kwargs),
+        }
+    return inner
+
+
+@wrap_result
+def _get_block_by_something(method, params):
+    block_identifier = params[0]
+    if block_identifier == 'latest' or block_identifier == '0x0000000000000000000000000000000000000000000000000000000000000005':
+        return {
+            'hash': '0x0000000000000000000000000000000000000000000000000000000000000005',
+            'number': 5,
+            'parentHash': '0x0000000000000000000000000000000000000000000000000000000000000004',
+            'transactions': [
+                {'gasPrice': 70},
+                {'gasPrice': 60},
+            ],
+            'miner': '0xA',
+            'timestamp': 100,
+        }
+    elif block_identifier == '0x0000000000000000000000000000000000000000000000000000000000000004':
+        return {
+            'hash': '0x0000000000000000000000000000000000000000000000000000000000000004',
+            'number': 4,
+            'parentHash': '0x0000000000000000000000000000000000000000000000000000000000000003',
+            'transactions': [
+                {'gasPrice': 100},
+                {'gasPrice': 80},
+                {'gasPrice': 60},
+            ],
+            'miner': '0xB',
+            'timestamp': 80,
+        }
+    elif block_identifier == '0x0000000000000000000000000000000000000000000000000000000000000003':
+        return {
+            'hash': '0x0000000000000000000000000000000000000000000000000000000000000003',
+            'number': 3,
+            'parentHash': '0x0000000000000000000000000000000000000000000000000000000000000002',
+            'transactions': [
+                {'gasPrice': 10},
+                {'gasPrice': 50},
+                {'gasPrice': 100},
+            ],
+            'miner': '0xC',
+            'timestamp': 60,
+        }
+    elif block_identifier == '0x0000000000000000000000000000000000000000000000000000000000000002':
+        return {
+            'hash': '0x0000000000000000000000000000000000000000000000000000000000000002',
+            'number': 2,
+            'parentHash': '0x0000000000000000000000000000000000000000000000000000000000000001',
+            'transactions': [
+                {'gasPrice': 50},
+                {'gasPrice': 70},
+                {'gasPrice': 100},
+            ],
+            'miner': '0xB',
+            'timestamp': 40,
+        }
+    elif block_identifier == '0x0000000000000000000000000000000000000000000000000000000000000001':
+        return {
+            'hash': '0x0000000000000000000000000000000000000000000000000000000000000001',
+            'number': 1,
+            'parentHash': '0x0000000000000000000000000000000000000000000000000000000000000000',
+            'transactions': [
+                {'gasPrice': 30},
+                {'gasPrice': 35},
+                {'gasPrice': 65},
+            ],
+            'miner': '0xA',
+            'timestamp': 20,
+        }
+    elif block_identifier == '0x0000000000000000000000000000000000000000000000000000000000000000' or block_identifier == 0:
+        return {
+            'hash': '0x0000000000000000000000000000000000000000000000000000000000000000',
+            'number': 0,
+            'parentHash': None,
+            'transactions': [
+                {'gasPrice': 30},
+                {'gasPrice': 50},
+                {'gasPrice': 60},
+            ],
+            'miner': '0xA',
+            'timestamp': 0,
+        }
+    else:
+        assert False
+
+
+@pytest.mark.parametrize(
+    'strategy_params,expected',
+    (
+        (dict(max_wait_seconds=60, sample_size=5, probability=98), 58),
+        (dict(max_wait_seconds=60, sample_size=5, probability=90), 46),
+        (dict(max_wait_seconds=60, sample_size=5, probability=50), 11),
+        (dict(max_wait_seconds=40, sample_size=5, probability=90), 56),
+        (dict(max_wait_seconds=20, sample_size=5, probability=90), 60),
+        (dict(max_wait_seconds=80, sample_size=5, probability=90), 36),
+    ),
+)
+def test_time_based_gas_price_strategy(strategy_params, expected):
+    fixture_middleware = construct_fixture_middleware({
+        'eth_getBlockByHash': _get_block_by_something,
+        'eth_getBlockByNumber': _get_block_by_something,
+    })
+
+    w3 = Web3(
+        providers=[BaseProvider()],
+        middlewares=[fixture_middleware],
+    )
+
+    time_based_gas_price_strategy = construct_time_based_gas_price_strategy(
+        **strategy_params,
+    )
+    w3.eth.setGasPriceStrategy(time_based_gas_price_strategy)
+    actual = w3.eth.generateGasPrice()
+    assert actual == expected
---
+++ web3/gas_strategies/time_based.py
@@ -0,0 +1,152 @@
+import operator
+import collections
+import math
+
+from cytoolz import (
+    curry,
+)
+
+from cytoolz import (
+    groupby,
+    sliding_window,
+)
+
+from eth_utils import (
+    to_tuple,
+)
+
+
+MinerData = collections.namedtuple('MinerData', ['miner', 'num_blocks', 'min_gas_price'])
+Probability = collections.namedtuple('Probability', ['gas_price', 'prob'])
+
+
+def get_avg_block_time(w3, sample_size):
+    latest = w3.eth.getBlock('latest')
+    oldest = w3.eth.getBlock(max(0, latest['number'] - sample_size))
+    return (latest['timestamp'] - oldest['timestamp']) / sample_size
+
+
+def get_raw_miner_data(w3, sample_size):
+    latest = w3.eth.getBlock('latest', full_transactions=True)
+
+    for transaction in latest['transactions']:
+        yield (latest['miner'], latest['hash'], transaction['gasPrice'])
+
+    block = latest
+
+    for _ in range(sample_size - 1):
+        if block['number'] == 0:
+            break
+        # we intentionally trace backwards using parent hashes rather than
+        # block numbers to make caching the data easier to implement.
+        block = w3.eth.getBlock(block['parentHash'], full_transactions=True)
+        for transaction in block['transactions']:
+            yield (block['miner'], block['hash'], transaction['gasPrice'])
+
+
+def aggregate_miner_data(raw_data):
+    data_by_miner = groupby(1, raw_data)
+
+    for miner, miner_data in data_by_miner.items():
+        _, block_hashes, gas_prices = map(set, zip(*miner_data))
+        yield MinerData(miner, len(set(block_hashes)), min(gas_prices))
+
+
+@to_tuple
+def compute_probabilities(miner_data, wait_blocks, sample_size):
+    """
+    Computes the probabilities that a txn will be accepted at each of the gas
+    prices accepted by the miners.
+    """
+    miner_data_by_price = tuple(sorted(
+        miner_data,
+        key=operator.attrgetter('min_gas_price'),
+        reverse=True,
+    ))
+    for idx in range(len(miner_data_by_price)):
+        min_gas_price = miner_data_by_price[idx].min_gas_price
+        num_blocks_accepting_price = sum(m.num_blocks for m in miner_data_by_price[idx:])
+        inv_prob_per_block = (sample_size - num_blocks_accepting_price) / sample_size
+        probability_accepted = 1 - inv_prob_per_block ** wait_blocks
+        yield Probability(min_gas_price, probability_accepted)
+
+
+def compute_gas_price(probabilities, desired_probability):
+    first = probabilities[0]
+    last = probabilities[-1]
+
+    if desired_probability >= first.prob:
+        return first.gas_price
+    elif desired_probability <= last.prob:
+        return last.gas_price
+
+    for left, right in sliding_window(2, probabilities):
+        if desired_probability < right.prob:
+            continue
+        elif desired_probability > left.prob:
+            raise Exception('Invariant')
+
+        adj_prob = desired_probability - right.prob
+        window_size = left.prob - right.prob
+        position = adj_prob / window_size
+        gas_window_size = left.gas_price - right.gas_price
+        gas_price = int(math.ceil(right.gas_price + gas_window_size * position))
+        return gas_price
+    else:
+        raise Exception('Invariant')
+
+
+@curry
+def construct_time_based_gas_price_strategy(max_wait_seconds,
+                                            sample_size=120,
+                                            probability=98):
+    """
+    A gas pricing strategy that uses recently mined block data to derive a gas
+    price for which a transaction is likely to be mined within X seconds with
+    probability P.
+
+    :param max_wait_seconds: The desired maxiumum number of seconds the
+        transaction should take to mine.
+    :param sample_size: The number of recent blocks to sample
+    :param probability: An integer representation of the desired probability
+        that the transaction will be mined within ``max_wait_seconds``.  0 means 0%
+        and 100 means 100%.
+    """
+    def time_based_gas_price_strategy(web3, transaction_params):
+        avg_block_time = get_avg_block_time(web3, sample_size=sample_size)
+        wait_blocks = int(math.ceil(max_wait_seconds / avg_block_time))
+
+        raw_miner_data = get_raw_miner_data(web3, sample_size=sample_size)
+        miner_data = aggregate_miner_data(raw_miner_data)
+
+        probabilities = compute_probabilities(
+            miner_data,
+            wait_blocks=wait_blocks,
+            sample_size=sample_size,
+        )
+
+        gas_price = compute_gas_price(probabilities, probability / 100)
+        return gas_price
+    return time_based_gas_price_strategy
+
+
+# fast: mine within 1 minute
+fast_gas_price_strategy = construct_time_based_gas_price_strategy(
+    max_wait_seconds=60,
+    sample_size=120,
+)
+# medium: mine within 10 minutes
+medium_gas_price_strategy = construct_time_based_gas_price_strategy(
+    max_wait_seconds=600,
+    sample_size=120,
+)
+# slow: mine within 1 hour (60 minutes)
+slow_gas_price_strategy = construct_time_based_gas_price_strategy(
+    max_wait_seconds=60 * 60,
+    sample_size=120,
+)
+# glacial: mine within the next 24 hours.
+glacial_gas_price_strategy = construct_time_based_gas_price_strategy(
+    max_wait_seconds=24 * 60 * 60,
+    sample_size=720,
+)
