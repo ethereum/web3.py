0f8161dad24259ccd1f88fd0820537dcc00276a3
Jason Carver <ut96caarrs@snkmail.com>
2018-01-08 11:47:04 -0800

    replace ethtestrpc tests with py-evm, new gas vals

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ setup(
     ],
     setup_requires=['setuptools-markdown'],
     extras_require={
-        'tester': ["eth-testrpc>=1.3.3,<2.0.0"],
+        'tester': ["py-evm>=0.2.0a8,<1.0"],
+        'testrpc': ["eth-testrpc>=1.3.3,<2.0.0"],
         'platform_system=="Windows"': [
             'pypiwin32'  # TODO: specify a version number, move under install_requires
         ],
