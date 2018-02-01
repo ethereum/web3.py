6c0ed1dcd0b05debe2644247a8be6561f60d381b
Mo <mnokhb@gmail.com>
2018-02-02 01:54:16 +0800

    Resend tx api: make ethereum tester auto mine disable a decorator

--- tests/integration/test_ethereum_tester.py
+++ tests/integration/test_ethereum_tester.py
@@ -176,71 +176,55 @@ def not_implemented(method, exc_type=NotImplementedError):
             method(*args, **kwargs)
     return inner
 
+def disable_auto_mine(func):
+    @functools.wraps(func)
+    def func_wrapper(self, eth_tester, web3, unlocked_account):
+        eth_tester.disable_auto_mine_transactions()
+        try:
+            func(self, eth_tester, web3, unlocked_account)
+        finally:
+            eth_tester.enable_auto_mine_transactions()
+            eth_tester.mine_block()
+    return func_wrapper
 
 class TestEthereumTesterEthModule(EthModuleTest):
     test_eth_sign = not_implemented(EthModuleTest.test_eth_sign, ValueError)
 
+    @disable_auto_mine
     def test_eth_getTransactionReceipt_unmined(self, eth_tester, web3, unlocked_account):
-        eth_tester.disable_auto_mine_transactions()
-        try:
-            super(TestEthereumTesterEthModule, self).test_eth_getTransactionReceipt_unmined(
-                web3, unlocked_account,
-            )
-        finally:
-            eth_tester.enable_auto_mine_transactions()
-            eth_tester.mine_block()
+        super(TestEthereumTesterEthModule, self).test_eth_getTransactionReceipt_unmined(
+            web3, unlocked_account,
+        )
 
-    # TODO: replace auto mining switch with decorator
-    # TODO: replicate for goethereum tests
+    @disable_auto_mine
     def test_eth_replaceTransaction(self, eth_tester, web3, unlocked_account):
-        eth_tester.disable_auto_mine_transactions()
-        try:
-            super(TestEthereumTesterEthModule, self).test_eth_replaceTransaction(
-                web3, unlocked_account,
-            )
-        finally:
-            eth_tester.enable_auto_mine_transactions()
-            eth_tester.mine_block()
+        super(TestEthereumTesterEthModule, self).test_eth_replaceTransaction(
+            web3, unlocked_account,
+        )
 
+    @disable_auto_mine
     def test_eth_replaceTransaction_incorrect_nonce(self, eth_tester, web3, unlocked_account):
-        eth_tester.disable_auto_mine_transactions()
-        try:
-            super(TestEthereumTesterEthModule, self).test_eth_replaceTransaction_incorrect_nonce(
-                web3, unlocked_account,
-            )
-        finally:
-            eth_tester.enable_auto_mine_transactions()
-            eth_tester.mine_block()
+        super(TestEthereumTesterEthModule, self).test_eth_replaceTransaction_incorrect_nonce(
+            web3, unlocked_account,
+        )
 
+    @disable_auto_mine
     def test_eth_replaceTransaction_gas_price_too_low(self, eth_tester, web3, unlocked_account):
-        eth_tester.disable_auto_mine_transactions()
-        try:
-            super(TestEthereumTesterEthModule, self).test_eth_replaceTransaction_gas_price_too_low(
-                web3, unlocked_account,
-            )
-        finally:
-            eth_tester.enable_auto_mine_transactions()
-            eth_tester.mine_block()
+        super(TestEthereumTesterEthModule, self).test_eth_replaceTransaction_gas_price_too_low(
+            web3, unlocked_account,
+        )
 
+    @disable_auto_mine
     def test_eth_replaceTransaction_gas_price_defaulting(self, eth_tester, web3, unlocked_account):
-        eth_tester.disable_auto_mine_transactions()
-        try:
-            super(TestEthereumTesterEthModule, self).test_eth_replaceTransaction_gas_price_defaulting(
-                web3, unlocked_account,
-            )
-        finally:
-            eth_tester.enable_auto_mine_transactions()
-            eth_tester.mine_block()
+        super(TestEthereumTesterEthModule, self).test_eth_replaceTransaction_gas_price_defaulting(
+            web3, unlocked_account,
+        )
 
+    @disable_auto_mine
     def test_eth_modifyTransaction(self, eth_tester, web3, unlocked_account):
-        eth_tester.disable_auto_mine_transactions()
-        try:
-            super(TestEthereumTesterEthModule, self).test_eth_modifyTransaction(
-                web3, unlocked_account,
-            )
-        finally:
-            eth_tester.enable_auto_mine_transactions()
-            eth_tester.mine_block()
+        super(TestEthereumTesterEthModule, self).test_eth_modifyTransaction(
+            web3, unlocked_account,
+        )
 
 class TestEthereumTesterVersionModule(VersionModuleTest):
     pass
