7bfa2ac5d45d274ac7e4a50415cf02eee140cdd3
Jason Carver <ut96caarrs@snkmail.com>
2018-02-06 15:52:41 -0800

    add pypy3 as a supported environment, by default

--- setup.py
+++ setup.py
@@ -62,5 +62,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy3',
     ],
 )
