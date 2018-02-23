edc1c112a40d01ab3e7957d60307b57c6e34c89d
Jason Carver <ut96caarrs@snkmail.com>
2018-02-22 17:25:27 -0800

    bugfix tests with old API: middleware_stack.insert

--- tests/core/eth-module/test_poa.py
+++ tests/core/eth-module/test_poa.py
@@ -11,7 +11,7 @@ def test_long_extra_data(web3):
     return_block_with_long_extra_data = construct_fixture_middleware({
         'eth_getBlockByNumber': {'extraData': '0x' + 'ff' * 33},
     })
-    web3.middleware_stack.insert(0, return_block_with_long_extra_data)
+    web3.middleware_stack.inject(return_block_with_long_extra_data, layer=0)
     with pytest.raises(ValueError):
         web3.eth.getBlock('latest')
 
@@ -20,7 +20,7 @@ def test_full_extra_data(web3):
     return_block_with_long_extra_data = construct_fixture_middleware({
         'eth_getBlockByNumber': {'extraData': '0x' + 'ff' * 32},
     })
-    web3.middleware_stack.insert(0, return_block_with_long_extra_data)
+    web3.middleware_stack.inject(return_block_with_long_extra_data, layer=0)
     block = web3.eth.getBlock('latest')
     assert block.extraData == b'\xff' * 32
 
@@ -29,8 +29,8 @@ def test_geth_proof_of_authority(web3):
     return_block_with_long_extra_data = construct_fixture_middleware({
         'eth_getBlockByNumber': {'extraData': '0x' + 'ff' * 33},
     })
-    web3.middleware_stack.insert(0, geth_poa_compatibility)
-    web3.middleware_stack.insert(0, return_block_with_long_extra_data)
+    web3.middleware_stack.inject(geth_poa_compatibility, layer=0)
+    web3.middleware_stack.inject(return_block_with_long_extra_data, layer=0)
     block = web3.eth.getBlock('latest')
     assert 'extraData' not in block
     assert block.proofOfAuthorityData == b'\xff' * 33
