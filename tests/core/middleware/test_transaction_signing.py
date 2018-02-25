5782a3f20f4870dc305ea6d25781141afc8e5bfe
Dylan Wilson <dylanjw@protonmail.com>
2018-02-24 17:47:53 -0800

    Make review requested updates
    - shorter middleware name
    - more tests
    - require from address in transactions signed by middleware

--- tests/core/middleware/test_transaction_signing.py
+++ tests/core/middleware/test_transaction_signing.py
@@ -1,19 +1,21 @@
 import pytest
-
-from web3 import Web3
-from web3.providers.eth_tester import EthereumTesterProvider
-
 from unittest.mock import (
     Mock,
 )
 
-from hexbytes import HexBytes
-
 import eth_account
 import eth_keys
+from hexbytes import (
+    HexBytes,
+)
+
+from web3 import Web3
 from web3.middleware.signing import (
-    construct_in_flight_transaction_signing_middleware,
+    construct_sign_and_send_raw_middleware,
     key_to_account,
+)
+from web3.providers.eth_tester import (
+    EthereumTesterProvider,
 )
 
 KEYFILE_DATA = '{"address":"dc544d1aa88ff8bbd2f2aec754b1f1e99e1812fd","crypto":{"cipher":"aes-128-ctr","ciphertext":"52e06bc9397ea9fa2f0dae8de2b3e8116e92a2ecca9ad5ff0061d1c449704e98","cipherparams":{"iv":"aa5d0a5370ef65395c1a6607af857124"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"9fdf0764eb3645ffc184e166537f6fe70516bf0e34dc7311dea21f100f0c9263"},"mac":"4e0b51f42b865c15c485f4faefdd1f01a38637e5247f8c75ffe6a8c0eba856f6"},"id":"5a6124e0-10f1-4c1c-ae3e-d903eacb740a","version":3}'  # noqa: E501
@@ -42,10 +44,11 @@ def key_object(request, private_key):
 @pytest.fixture
 def mocked_signing_middleware(private_key):
     make_request, web3 = Mock(), Mock()
-    signing_middleware = construct_in_flight_transaction_signing_middleware(private_key)
+    signing_middleware = construct_sign_and_send_raw_middleware(private_key)
     middleware = signing_middleware(make_request, web3)
     middleware.make_request = make_request
     middleware.web3 = web3
+    middleware.web3.net.version = 1
     return middleware
 
 
@@ -54,19 +57,24 @@ def test_key_to_account(w3, key_object):
     assert isinstance(account, eth_account.local.LocalAccount)
 
 
-def test_key_to_account_value_error(w3):
-    with pytest.raises(ValueError):
+def test_key_to_account_type_error(w3):
+    with pytest.raises(TypeError):
         key_to_account(w3, 1234567890)
 
 
 def test_captured_method(private_key, mocked_signing_middleware):
+    account = eth_account.Account.privateKeyToAccount(private_key)
+    mocked_signing_middleware.web3.eth.account.privateKeyToAccount().address = \
+        account.address
     method = 'eth_sendTransaction'
     params = [{
-        'to': '0x0',
+        'to': "0x00000000000000000000000000000000",
+        'from': account.address,
         'value': 1,
     }]
     mocked_signing_middleware(method, params)
-    mocked_signing_middleware.web3.eth.account.privateKeyToAccount.assert_called_with(private_key)
+    assert 'eth_sendRawTransaction' in \
+        mocked_signing_middleware.make_request.call_args[0]
 
 
 def test_passthrough_method(private_key, mocked_signing_middleware):
@@ -76,16 +84,61 @@ def test_passthrough_method(private_key, mocked_signing_middleware):
     mocked_signing_middleware.make_request.called_with(params)
 
 
-def test_signed_transaction(w3, key_object):
+@pytest.fixture(scope='function')
+def fund_account(w3, private_key):
     # fund local account
-    account = key_to_account(w3, key_object)
+    account = key_to_account(w3, private_key)
     w3.eth.sendTransaction({
         'to': account.address,
         'from': w3.eth.accounts[0],
         'value': 10})
     assert w3.eth.getBalance(account.address) == 10
-    w3.middleware_stack.add(construct_in_flight_transaction_signing_middleware(key_object))
+
+
+def test_signed_transaction_with_set_gas(w3, key_object, fund_account):
+    account = key_to_account(w3, key_object)
+    w3.middleware_stack.add(construct_sign_and_send_raw_middleware(key_object))
     w3.eth.sendTransaction({
         'to': w3.eth.accounts[0],
-        'value': 10})
+        'from': account.address,
+        'gas': 2100,
+        'gasPrice': 0,
+        'value': 1})
     assert w3.eth.getBalance(account.address) == 0
+
+
+def test_signed_transaction_unset_gas(w3, key_object, fund_account):
+    account = key_to_account(w3, key_object)
+    w3.middleware_stack.add(construct_sign_and_send_raw_middleware(key_object))
+    account.address
+    w3.eth.sendTransaction({
+        'to': w3.eth.accounts[0],
+        'from': account.address,
+        'value': 1})
+    assert w3.eth.getBalance(account.address) == 0
+
+
+def test_wrong_address_signed_transaction(w3, key_object):
+    w3.middleware_stack.add(construct_sign_and_send_raw_middleware(key_object))
+    with pytest.raises(ValueError):
+        w3.eth.sendTransaction({
+            'to': w3.eth.accounts[0],
+            'from': w3.eth.accounts[1],
+            'value': 10})
+
+
+def test_invalid_address_signed_transaction(w3, key_object):
+    w3.middleware_stack.add(construct_sign_and_send_raw_middleware(key_object))
+    with pytest.raises(ValueError):
+        w3.eth.sendTransaction({
+            'to': w3.eth.accounts[0],
+            'from': '!@#',
+            'value': 10})
+
+
+def test_missing_address_signed_transaction(w3, key_object):
+    w3.middleware_stack.add(construct_sign_and_send_raw_middleware(key_object))
+    with pytest.raises(ValueError):
+        w3.eth.sendTransaction({
+            'to': w3.eth.accounts[0],
+            'value': 10})
--- web3/middleware/signing.py
+++ web3/middleware/signing.py
@@ -1,44 +1,59 @@
-from web3.utils.transactions import (
-    fill_transaction_defaults
-)
 import eth_account
-from hexbytes import HexBytes
+from eth_utils import (
+    is_same_address,
+)
 
-from cytoolz import (
-    merge,
+from web3.utils.transactions import (
+    fill_transaction_defaults,
 )
 
 
 def key_to_account(web3, key):
     if isinstance(key, eth_account.local.LocalAccount):
         return key
-    if isinstance(key, (HexBytes, str, bytes)):
+    if isinstance(key, (str, bytes)):
         return web3.eth.account.privateKeyToAccount(key)
     try:
-        # TODO Find a better way to check for and handle eth_keys objects
-        return web3.eth.account.privateKeyToAccount(key.to_hex())
+        sk_hex = key.to_hex()
+        return web3.eth.account.privateKeyToAccount(sk_hex)
     except AttributeError:
-        raise ValueError("key must be one of the types (...). Was of type {0}".format(type(key)))
+        raise TypeError("key must be one of the types (...). Was of type {0}".format(type(key)))
 
 
-def construct_in_flight_transaction_signing_middleware(private_key):
+def construct_sign_and_send_raw_middleware(private_key):
 
-    def in_flight_transaction_signing_middleware(make_request, web3):
+    def sign_and_send_raw_middleware(make_request, web3):
         account = key_to_account(web3, private_key)
 
         def middleware(method, params):
             if method == "eth_sendTransaction":
+                transaction = fill_transaction_defaults(web3, params[0])
 
-                transaction_filler = {
-                    'from': account.address,
-                    'nonce': web3.eth.getTransactionCount(account.address)
-                }
+                if 'from' in transaction:
+                    try:
+                        assert is_same_address(account.address, transaction['from'])
+                    except AssertionError:
+                        raise ValueError("Sending account address mismatch. \
+                            Transaction 'from' parameter does not match the \
+                            address to the private key used to construct signing \
+                            middleware.")
+                else:
+                    raise ValueError("Transaction signing middleware parameter \
+                        requirement not met: 'from' address was not included in\
+                        transaction parameters.")
 
-                transaction = merge(transaction_filler, params[0])
-                filled_transaction = fill_transaction_defaults(web3, transaction)
-                signed_transaction = account.signTransaction(filled_transaction)
-                return make_request("eth_sendRawTransaction", [signed_transaction.rawTransaction])
+                # TODO: Can this be added to fill_transaction_defaults()?
+                transaction['nonce'] = web3.eth.getTransactionCount(account.address)
+
+                signed_transaction = account.signTransaction(transaction)
+
+                return make_request(
+                    "eth_sendRawTransaction",
+                    [signed_transaction.rawTransaction])
+
             else:
                 return make_request(method, params)
+
         return middleware
-    return in_flight_transaction_signing_middleware
+
+    return sign_and_send_raw_middleware
