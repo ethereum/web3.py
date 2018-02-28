9813a52d4dc8ab9f3b03121204e5ccc2af89ef29
Jason Carver <ut96caarrs@snkmail.com>
2018-02-28 13:31:26 -0800

    comment explaining geth 1.8 error normalization

--- web3/middleware/normalize_errors.py
+++ web3/middleware/normalize_errors.py
@@ -7,6 +7,11 @@ from cytoolz import (
 def normalize_errors_middleware(make_request, web3):
     def middleware(method, params):
         result = make_request(method, params)
+
+        # As of v1.8, Geth returns errors when you request a
+        # receipt for a transaction that is not in the chain.
+        # It used to return a result of None, so we simulate the old behavior.
+
         if method == 'eth_getTransactionReceipt' and 'error' in result:
             is_geth = web3.version.node.startswith('Geth')
             if is_geth and result['error']['code'] == -32000:
