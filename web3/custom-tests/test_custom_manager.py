725d95719f2a04da3a75e3863f764a0d997f1967
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2018-03-29 18:10:10 -0700

    add imports, add testign code for providers check

--- web3/custom-tests/test_custom_manager.py
+++ web3/custom-tests/test_custom_manager.py
@@ -6,15 +6,36 @@
 import pytest
 
 from web3 import (
-
     Web3, HTTPProvider
+)
 
+from web3.custom_managers import (
+    check_unique_providers,
+    verify_provider_network,
+    threaded_provider_ranking,
+    ManagerMixin,
+    RankingRequestManager
 )
 
 
 def test_check_unique_providers():
     ''' '''
-    pass
+    uris = ['http://127.0.0.1:8545',
+            'http://52.0.0.1:8545',
+            'http://192.0.0.1:8545',
+            'http://ropsten.infura,io'
+            ]
+    providers = [HTTPProvider(uri) for uri in uris]
+    web3 = Web3(providers)
+    check_unique_providers(web3.providers)
+    assert len(uris) == len(web3.providers)
+
+    uris_2 = uris * 2
+    providers = [HTTPProvider(uri) for uri in uris_2]
+    web3 = Web3(providers)
+    assert len(uris_2) == len(web3.providers)
+    check_unique_providers(web3.providers)
+    assert len(uris_2) == len(web3.providers) * 2
 
 
 def test_verify_provider_network():
