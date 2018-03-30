12236211272f6f39edd947509507638c69c4d621
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2018-03-29 17:48:41 -0700

    init custom manager tests

---
+++ web3/custom-tests/test_custom_manager.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+'''
+    dev tests for ranked-provider manager -- to be moved after dev.
+'''
+import pytest
+
+from web3 import (
+
+    Web3, HTTPProvider
+
+)
+
+
+def test_check_unique_providers():
+    ''' '''
+    pass
+
+
+def test_verify_provider_network():
+    pass
+
+
+def test_threaded_provider_ranking():
+    pass
+
+
+# ManagerMixin tests
+
+# __init__
+
+# _setup
+
+# _validate_provider_strategy
+
+# _validate_polling_request
+
+# _update_last_provider_polling
+
+# toggle_provider_strategy
+
+# which_provider_strategy
+
+# RankingRequestManager
+# __init__
+# Mixin quick test
+# Manager quick tests
+# _make_requesst
