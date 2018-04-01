50e22e3b11f7eddd2caf0a9e4e2debf2782dc551
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2018-04-01 01:54:45 -0700

    add entry point tests

--- web3/custom-tests/test_web3_entry.py
+++ web3/custom-tests/test_web3_entry.py
@@ -11,37 +11,62 @@ from web3 import (
 
 )
 
+from web3.manager import (
+    RequestManager
+)
+
+from web3.custom_managers import (
+    RankingRequestManager
+)
+
 
 def test_web3_entry():
     ''' tests for revised Web3 entry '''
+    providers = [HTTPProvider('https://ropsten.infura.io')]
 
     # original Web3 entry without extended signature calling on original manager
-    web3 = Web3(HTTPProvider('https://ropsten.infura.io'))
+    web3 = Web3(providers)
     assert web3
+    assert web3.isConnected()
 
     # original Web3 entry with extended signature defaulting to original manager
+    # straight pass through
     provider_strategy = 'default'
     ranking_strategy = 'default'
-    web3 = Web3(HTTPProvider('https://ropsten.infura.io'))
+    web3 = Web3(providers)
     assert web3
+    assert web3.isConnected()
+    assert isinstance(web3.manager, RequestManager)
 
+    # main raise ValueError
     provider_strategy = 'boo hoo'
     ranking_strategy = 'default'
-    web3 = Web3(HTTPProvider('https://ropsten.infura.io'), provider_strategy, ranking_strategy)
-    # assert web3
+    with pytest.raises(ValueError):
+        web3 = Web3(providers, provider_type=provider_strategy, provider_strategy=ranking_strategy)
 
+    # custom manager raise ValueError
     provider_strategy = 'default'
     ranking_strategy = 'boo yah'
-    web3 = Web3(HTTPProvider('https://ropsten.infura.io'), provider_strategy, ranking_strategy)
-    # assert web3
+    # with pytest.raises(ValueError):
+    #    web3 = Web3(providers, provider_type=provider_strategy, provider_strategy=ranking_strategy)
 
-    # ranked-provider Web3 entry calling on new manager
-    provider_strategy = 'ranking_provider'
-    ranking_strategy = 'default'
-    web3 = Web3(HTTPProvider('https://ropsten.infura.io'), provider_strategy, ranking_strategy)
+    # ranked-provider Web3 entry calling on new manager with default ranking_strategy
+    # """
+    # provider_strategy = 'ranking_provider'
+    # ranking_strategy = 'default'
+    # web3 = Web3(providers, provider_type=provider_strategy, provider_strategy=ranking_strategy)
+    # del web3
     # assert web3
+    # """
 
+    # finally, call on the custom manager
+    providers = [HTTPProvider('https://ropsten.infura.io'), HTTPProvider('https://ropsten.infura.io')]
     provider_strategy = 'ranking_provider'
     ranking_strategy = 'by_highest_block'
-    web3 = Web3(HTTPProvider('https://ropsten.infura.io'), provider_strategy, ranking_strategy)
-    # assert web3
+    web3 = Web3(providers, provider_type=provider_strategy, provider_strategy=ranking_strategy)
+    assert web3
+    assert isinstance(web3.manager, RankingRequestManager)
+    assert web3.isConnected()
+    assert hasattr(web3.manager, 'toggle_provider_strategy')
+    assert hasattr(web3.manager, 'which_provider_strategy')
+    # assert web3.manager.which_provider_strategy == ranking_strategy
