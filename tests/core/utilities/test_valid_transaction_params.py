5b7dfbe616e4057211413fd2a718b3dd209dbde4
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2018-04-26 17:52:05 -0700

    change tests to parameterized tests.

--- tests/core/utilities/test_valid_transaction_params.py
+++ tests/core/utilities/test_valid_transaction_params.py
@@ -38,31 +38,47 @@ def test_assert_valid_transaction_params_invalid_param():
 
 
 def test_extract_valid_transaction_params():
-    from copy import deepcopy
-    base_input = {
+    ''' baseline test for clean param dict '''
+    input = {
         'from': '0x0',
         'to': '0x0',
         'gas': 21000,
         'gasPrice': 5000000,
+        'data': '0x0',
         'value': 1,
         'nonce': 2,
         'chainId': 1,
     }
-
-    input = deepcopy(base_input)
-    input['data'] = '0x0'
     valid_transaction_params = extract_valid_transaction_params(input)
     assert valid_transaction_params == input
 
-    in_input = deepcopy(base_input)
-    in_input['input'] = '0x0'
-    in_input['data'] = '0x0'
-    valid_transaction_params = extract_valid_transaction_params(in_input)
-    assert valid_transaction_params == input
 
-    input['input'] = '0x1'
-    with pytest.raises(AttributeError, match=r'.* You need to resolve this conflict.'):
-        extract_valid_transaction_params(input)
+test_data = [({'data': '0x0', 'input': '0x0'}, {'data': '0x0'}), ]
+@pytest.mark.parametrize("transaction_params, expected", test_data)  # noqa E302
+def test_extract_valid_transaction_params_2(transaction_params, expected):
+    valid_transaction_params = extract_valid_transaction_params(transaction_params)
+    assert valid_transaction_params == expected
+
+
+test_data = [({'input': '0x0'}, {'data': '0x0'}), ]
+@pytest.mark.parametrize("transaction_params, expected", test_data)  # noqa E302
+def test_extract_valid_transaction_params_3(transaction_params, expected):
+    valid_transaction_params = extract_valid_transaction_params(transaction_params)
+    assert valid_transaction_params == expected
+
+
+test_data = [({}, {}), ]
+@pytest.mark.parametrize("transaction_params, expected", test_data)  # noqa E302
+def test_extract_valid_transaction_params_4(transaction_params, expected):
+    valid_transaction_params = extract_valid_transaction_params(transaction_params)
+    assert valid_transaction_params == expected
+
+
+
+@pytest.mark.xfail(raises=AttributeError, strict=True)  # noqa E302
+def test_extract_valid_transaction_params_5():
+    transaction_params = {'data': '0x0', 'input': '0x1'}
+    extract_valid_transaction_params(transaction_params)
 
 
 def test_extract_valid_transaction_params_includes_invalid():
