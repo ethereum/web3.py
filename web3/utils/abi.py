a24699bb5dbf9edf961ab21e93c52229c53471ed
Jason Carver <ut96caarrs@snkmail.com>
2018-05-09 15:04:50 -0700

    Squash deprecation warnings about escape chars

--- web3/utils/abi.py
+++ web3/utils/abi.py
@@ -273,9 +273,9 @@ BASE_TYPE_REGEX = '|'.join((
 ))
 
 SUB_TYPE_REGEX = (
-    '\['
+    r'\['
     '[0-9]*'
-    '\]'
+    r'\]'
 )
 
 TYPE_REGEX = (
@@ -331,7 +331,7 @@ def size_of_type(abi_type):
         return 8
     if abi_type == 'address':
         return 160
-    return int(re.sub("\D", "", abi_type))
+    return int(re.sub(r"\D", "", abi_type))
 
 
 END_BRACKETS_OF_ARRAY_TYPE_REGEX = r"\[[^]]*\]$"
@@ -380,7 +380,7 @@ NAME_REGEX = (
 ENUM_REGEX = (
     '^'
     '{lib_name}'
-    '\.'
+    r'\.'
     '{enum_name}'
     '$'
 ).format(lib_name=NAME_REGEX, enum_name=NAME_REGEX)
