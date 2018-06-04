0a83658b3ee41fdc8473e3156a5b7591ef992c9b
David Sanders <davesque@gmail.com>
2018-06-04 16:37:52 -0600

    Lint in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (
     find_packages,
 )
 
-extras_require={
+extras_require = {
     'test': [
         "pytest==3.3.2",
         "pytest-xdist",
@@ -28,10 +28,10 @@ extras_require={
 }
 
 extras_require['dev'] = (
-    extras_require['dev']
-    + extras_require['test']
-    + extras_require['lint']
-    + extras_require['doc']
+    extras_require['dev'] +
+    extras_require['test'] +
+    extras_require['lint'] +
+    extras_require['doc']
 )
 
 setup(
