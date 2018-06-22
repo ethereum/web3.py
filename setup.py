d63d5261c17de5526a83882c46226b36c1e93740
Jason Carver <ut96caarrs@snkmail.com>
2018-06-21 17:31:57 -0700

    Bump version: 4.3.0 → 4.4.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import (
 setup(
     name='web3',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='4.3.0',
+    version='4.4.0',
     description="""Web3.py""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
