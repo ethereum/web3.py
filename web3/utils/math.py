d7d16df9ad696ab375dd1c95b546061e2fbfe7d6
Benjamin Diamond <30356252+benediamond@users.noreply.github.com>
2018-07-04 16:22:26 -0400

    sort once

--- web3/utils/math.py
+++ web3/utils/math.py
@@ -5,7 +5,8 @@ def percentile(values, percentile):
     if not values:
         return None
     index = len(values) * percentile / 100
+    sorted_values = sorted(values)
     if index % 2 == 0:
-        return (sorted(values)[index] + sorted(values)[index + 1]) / 2
+        return (sorted_values[index] + sorted_values[index + 1]) / 2
 
-    return sorted(values)[math.ceil(index)]
+    return sorted_values[math.ceil(index)]
