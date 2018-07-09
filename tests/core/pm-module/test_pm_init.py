1648185c8b34897ad9d0d130dde9fb6525db1c7e
Nick Gheorghita <nickgheorghita@gmail.com>
2018-07-09 16:54:17 -0600

    Implement get_contract_factory, deploy and use contract

--- tests/core/pm-module/test_pm_init.py
+++ tests/core/pm-module/test_pm_init.py
@@ -4,6 +4,15 @@ from pathlib import (
 )
 import pytest
 
+<<<<<<< HEAD
+=======
+from ethpm.backends.ipfs import IPFSGatewayBackend
+from ethpm.exceptions import (
+    InsufficientAssetsError,
+)
+from solc import compile_source
+
+>>>>>>> Implement get_contract_factory, deploy and use contract
 from web3 import Web3
 from web3.pm import (
     PM,
@@ -26,6 +35,7 @@ V2_PACKAGES_DIR = Path(__file__).parent / 'packages'
 @pytest.fixture
 def web3():
     w3 = Web3(Web3.EthereumTesterProvider())
+    w3.eth.defaultAccount = w3.eth.accounts[0]
     PM.attach(w3, 'pm')
     return w3
 
@@ -33,6 +43,12 @@ def web3():
 @pytest.fixture
 def owned_manifest():
     with open(str(V2_PACKAGES_DIR / 'owned' / '1.0.0.json')) as file_obj:
+        return json.load(file_obj)
+
+
+@pytest.fixture
+def standard_token_manifest():
+    with open(str(V2_PACKAGES_DIR / 'standard-token' / '1.0.0.json')) as file_obj:
         return json.load(file_obj)
 
 
@@ -47,3 +63,18 @@ def test_get_contract_type_raises_insufficient_assets_error(web3, owned_manifest
     owned_package = web3.pm.get_package_from_manifest(owned_manifest)
     with pytest.raises(InsufficientAssetsError):
         owned_package.get_contract_factory('owned')
+
+
+def test_deploy_a_standalone_package_integration(web3, standard_token_manifest):
+    token_package = web3.pm.get_package_from_manifest(standard_token_manifest)
+    # Added deployment bytecode to manifest to be able to generate factory
+    ERC20 = token_package.get_contract_factory('StandardToken')
+    # totalSupply = 100
+    tx_hash = ERC20.constructor(100).transact()
+    tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+    address = tx_receipt["contractAddress"]
+    # do we want a `pm` api for this? 
+    # token_package.get_contract_instance(address)
+    erc20 = web3.eth.contract(address=address, abi=ERC20.abi)
+    total_supply = erc20.functions.totalSupply().call()
+    assert total_supply == 100
