12b31d7522006c6ba93a330cf92469507a30f3da
Jason Carver <ut96caarrs@snkmail.com>
2018-07-24 12:46:58 -0700

    Confirm support for latest eth-account release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "toolz>=0.9.0,<1.0.0;implementation_name=='pypy'",
         "cytoolz>=0.9.0,<1.0.0;implementation_name=='cpython'",
         "eth-abi>=1.1.1,<2",
-        "eth-account>=0.2.1,<0.3.0",
+        "eth-account>=0.2.1,<0.4.0",
         "eth-utils>=1.0.1,<2.0.0",
         "hexbytes>=0.1.0,<1.0.0",
         "lru-dict>=1.1.6,<2.0.0",
