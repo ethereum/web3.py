6a64fa8dcd8564d9724b41b962811158486e7548
Dylan Wilson <dylanjw@protonmail.com>
2018-08-16 15:40:14 -0700

    Fix test failures

--- tests/core/contracts/test_contract_util_functions.py
+++ tests/core/contracts/test_contract_util_functions.py
@@ -1,3 +1,8 @@
+from web3.contract import (
+    parse_block_identifier_int,
+)
+
+
 def test_parse_block_identifier_int(web3):
     last_num = web3.eth.getBlock('latest').number
-    assert web3.eth.getBlock(0) == web3.eth.getBlock(-1 - last_num)
+    assert 0 == parse_block_identifier_int(web3, -1 - last_num)
