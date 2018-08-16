3e8f2e405bd7ba9b090a17b91cadea99f98d6417
Dylan Wilson <dylanjw@protonmail.com>
2018-08-16 11:36:17 -0700

    Add request param normalization middleware
    Along with wrapping address in a list in filter parameter

---
+++ web3/middleware/normalize_request_parameters.py
@@ -0,0 +1,25 @@
+from eth_utils import (
+    is_string,
+)
+
+from web3.utils.formatters import (
+    apply_formatter_at_index,
+    apply_formatter_if,
+    apply_formatters_to_dict,
+)
+
+from .formatting import (
+    construct_formatting_middleware,
+)
+
+FILTER_PARAM_NORMALIZERS = apply_formatters_to_dict({
+    'address': apply_formatter_if(is_string, lambda x: [x])})
+
+METHOD_NORMALIZERS = {
+    'eth_getLogs': apply_formatter_at_index(FILTER_PARAM_NORMALIZERS, 0),
+    'eth_newFilter': apply_formatter_at_index(FILTER_PARAM_NORMALIZERS, 0)
+}
+
+request_parameter_normalizer = construct_formatting_middleware(
+    request_formatters=METHOD_NORMALIZERS,
+)
