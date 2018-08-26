3e726cd16a8c178040b1d835863e665cf5f4adbb
Mukundan <30190448+Mukundan314@users.noreply.github.com>
2018-08-26 11:43:09 +0530

    Fix #962

--- setup.py
+++ setup.py
@@ -5,43 +5,83 @@ from setuptools import (
     setup,
 )
 
+extras_require = {
+    'test': [
+        "eth-testrpc>=1.3.3,<2.0.0"
+        "eth-tester[py-evm]==0.1.0-beta.31",
+        "py-geth>=2.0.1,<3.0.0",
+    ],
+    'lint': [
+        "flake8==3.4.1",
+        "isort>=4.2.15,<5",
+    ],
+    'doc': [
+        #"mock"
+        #"sphinx-better-theme>=0.1.4"
+        "click>=5.1",
+        "configparser==3.5.0",
+        "contextlib2>=0.5.4",
+        #"eth-testrpc>=0.8.0",
+        #"ethereum-tester-client>=1.1.0",
+        "ethtoken",
+        "py-geth>=1.4.0",
+        "py-solc>=0.4.0",
+        "pytest>=2.7.2",
+        "sphinx",
+        "sphinx_rtd_theme>=0.1.9",
+        "toposort>=1.4",
+        "urllib3",
+        "web3>=2.1.0",
+        "wheel",
+    ],
+    'dev': [
+        "bumpversion",
+        "flaky>=3.3.0",
+        "hypothesis>=3.31.2",
+        "pytest>=3.5.0,<4",
+        "pytest-mock==1.*",
+        "pytest-pythonpath>=0.3",
+        "pytest-watch==4.*",
+        "pytest-xdist==1.*",
+        "tox>=1.8.0",
+        "tqdm",
+        "when-changed",
+    ],
+}
+
+extras_require['dev'] = (
+    extras_require['dev'] +
+    extras_require['test'] +
+    extras_require['lint'] +
+    extras_require['doc']
+)
 
 setup(
     name='web3',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
     version='4.6.0',
-    description="""Web3.py""",
+    description="""web3: A Python implementation of web3.js""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
     author_email='pipermerriam@gmail.com',
     url='https://github.com/ethereum/web3.py',
     include_package_data=True,
     install_requires=[
-        "toolz>=0.9.0,<1.0.0;implementation_name=='pypy'",
         "cytoolz>=0.9.0,<1.0.0;implementation_name=='cpython'",
         "eth-abi>=1.1.1,<3",
         "eth-account>=0.2.1,<0.4.0",
+        "eth-hash[pycryptodome]",
         "eth-utils>=1.0.1,<2.0.0",
         "hexbytes>=0.1.0,<1.0.0",
         "lru-dict>=1.1.6,<2.0.0",
-        "eth-hash[pycryptodome]",
-        "requests>=2.16.0,<3.0.0",
-        "websockets>=6.0.0,<7.0.0",
         "pypiwin32>=223;platform_system=='Windows'",
+        "requests>=2.16.0,<3.0.0",
+        "toolz>=0.9.0,<1.0.0;implementation_name=='pypy'",
+        "websockets>=6.0.0,<7.0.0",
     ],
     setup_requires=['setuptools-markdown'],
     python_requires='>=3.5, <4',
-    extras_require={
-        'tester': [
-            "eth-tester[py-evm]==0.1.0-beta.31",
-            "py-geth>=2.0.1,<3.0.0",
-        ],
-        'testrpc': ["eth-testrpc>=1.3.3,<2.0.0"],
-        'linter': [
-            "flake8==3.4.1",
-            "isort>=4.2.15,<5",
-        ],
-    },
+    extras_require=extras_require,
     py_modules=['web3', 'ens'],
     license="MIT",
     zip_safe=False,
