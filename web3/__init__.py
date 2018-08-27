f79f75c5e806df40d575e1767e0d7cb10dac18c7
Dylan Wilson <dylanjw@protonmail.com>
2018-08-27 15:38:04 -0700

    Add warning for python 3.5

--- web3/__init__.py
+++ web3/__init__.py
@@ -1,8 +1,17 @@
 import pkg_resources
 import sys
+import warnings
+
+if (3, 5) <= sys.version_info < (3, 6):
+    warnings.warn(
+        "Support for Python 3.5 will be removed in web3.py v5",
+        category=DeprecationWarning,
+        stacklevel=2)
 
 if sys.version_info < (3, 5):
-    raise EnvironmentError("Python 3.5 or above is required")
+    raise EnvironmentError(
+        "Python 3.5 or above is required. "
+        "Note that support for Python 3.5 will be remove in web3.py v5")
 
 from eth_account import Account  # noqa: E402
 from web3.main import Web3  # noqa: E402
