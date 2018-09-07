fafe2ae62cf32f2d52f1d049115633713a403a66
Julian Liu <liu.julian@gmail.com>
2018-09-07 09:33:21 +0000

    Pin eth-abi below version 2.0.0

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     install_requires=[
         "toolz>=0.9.0,<1.0.0;implementation_name=='pypy'",
         "cytoolz>=0.9.0,<1.0.0;implementation_name=='cpython'",
-        "eth-abi>=1.2.0,<3",
+        "eth-abi>=1.2.0,<2.0.0",
         "eth-account>=0.2.1,<0.4.0",
         "eth-utils>=1.0.1,<2.0.0",
         "hexbytes>=0.1.0,<1.0.0",
