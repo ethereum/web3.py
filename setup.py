c2bcc472520b9fedc9bf1260c264186a9498941f
Jason Carver <ut96caarrs@snkmail.com>
2018-09-11 09:38:13 -0700

    Bump version: 4.7.0 → 4.7.1

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ extras_require['dev'] = (
 setup(
     name='web3',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='4.7.0',
+    version='4.7.1',
     description="""Web3.py""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
