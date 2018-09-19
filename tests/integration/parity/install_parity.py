eadffd04c1c36763d7049fd957de824a66747786
Dylan Wilson <dylanjw@protonmail.com>
2018-09-18 18:11:17 -0700

    Bump parity test binary to 1.11.11

--- tests/integration/parity/install_parity.py
+++ tests/integration/parity/install_parity.py
@@ -20,7 +20,7 @@ VERSION_STRINGS = {
     "v1.8.8": "1_8_8",
     "v1.9.1": "1_9_1",
     "v1.10.4": "1_10_4",
-    "v1.11.7": "1_11_7",
+    "v1.11.11": "1_11_11",
 }
 ARCHITECTURE = 'x86_64'
 OS = os.getenv('PARITY_OS', 'debian')
