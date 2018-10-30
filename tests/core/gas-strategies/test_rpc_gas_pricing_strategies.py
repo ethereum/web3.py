71fa18e6dec257f1f43a585a00080e8fd6c36e6f
Dylan Wilson <dylanjw@protonmail.com>
2018-10-30 18:55:29 +0100

    Fix rpc_gas_pricing_strategies tests

--- tests/core/gas-strategies/test_rpc_gas_pricing_strategies.py
+++ tests/core/gas-strategies/test_rpc_gas_pricing_strategies.py
@@ -1,14 +1,17 @@
+import pytest
 from web3.gas_strategies.rpc import (
     rpc_gas_price_strategy,
 )
 
 
-def test_default_rpc_gas_price_strategy(web3):
-    assert rpc_gas_price_strategy(web3, {
+@pytest.mark.asyncio
+async def test_default_rpc_gas_price_strategy(web3):
+    assert await rpc_gas_price_strategy(web3, {
         'to': '0x0',
         'value': 1
     }) == 1
 
 
-def test_default_rpc_gas_price_strategy_callable_without_transaction(web3):
-    assert rpc_gas_price_strategy(web3) == 1
+@pytest.mark.asyncio
+async def test_default_rpc_gas_price_strategy_callable_without_transaction(web3):
+    assert await rpc_gas_price_strategy(web3) == 1
