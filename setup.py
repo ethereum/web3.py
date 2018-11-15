df03ed098f32054fecc6bb4d2cfd7cf1a524e4cc
Keri <kclowes@users.noreply.github.com>
2018-11-15 14:51:34 -0700

    Bump version: 4.8.1 → 4.8.2

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ extras_require['dev'] = (
 setup(
     name='web3',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='4.8.1',
+    version='4.8.2',
     description="""Web3.py""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
