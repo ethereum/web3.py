bdfc4b840dbbd09e4d7424e6c435f9802f6295f5
Andre Luckow <andre.luckow@gmail.com>
2019-01-27 20:37:18 +0000

    Added test

--- web3/parity.py
+++ web3/parity.py
@@ -35,7 +35,7 @@ class Parity(Module):
             "parity_netPeers",
             [],
         )
-    
+
     def addReservedPeer(self, params):
         return self.web3.manager.request_blocking(
             "parity_addReservedPeer",
