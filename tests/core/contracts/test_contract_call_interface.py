b7e6bb9757aa58e290f9fa7be449dbbd08298609
Keri <kclowes@users.noreply.github.com>
2019-01-30 15:33:59 -0700

    Whitespace

--- tests/core/contracts/test_contract_call_interface.py
+++ tests/core/contracts/test_contract_call_interface.py
@@ -28,7 +28,6 @@ from web3.exceptions import (
     ValidationError,
 )
 
-
 # Ignore warning in pyethereum 1.6 - will go away with the upgrade
 pytestmark = pytest.mark.filterwarnings("ignore:implicit cast from 'char *'")
 
