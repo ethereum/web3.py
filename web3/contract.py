3eb44b5fc261376a64c81e8bbc62309957a64e10
Keri <kclowes@users.noreply.github.com>
2019-01-30 12:00:55 -0700

    First pass on Contract caller

--- web3/contract.py
+++ web3/contract.py
@@ -101,6 +101,46 @@ DEPRECATED_SIGNATURE_MESSAGE = (
 ACCEPTABLE_EMPTY_STRINGS = ["0x", b"0x", "", b""]
 
 
+class ReaderMethod:
+    ALLOWED_MODIFIERS = {'call', 'estimateGas', 'transact', 'buildTransaction'}
+
+    def __init__(self, function, normalizers=None):
+        self._function = function
+
+    def __call__(self, *args, **kwargs):
+        return self.__prepared_function(*args, **kwargs)
+
+    def __prepared_function(self, *args, **kwargs):
+        if not kwargs:
+            modifier, modifier_dict = 'call', {}
+        elif len(kwargs) == 1:
+            modifier, modifier_dict = kwargs.popitem()
+            if modifier not in self.ALLOWED_MODIFIERS:
+                raise TypeError(
+                    "The only allowed keyword arguments are: %s" % self.ALLOWED_MODIFIERS)
+        else:
+            raise TypeError("Use up to one keyword argument, one of: %s" % self.ALLOWED_MODIFIERS)
+
+        return getattr(self._function(*args), modifier)(modifier_dict)
+
+class ContractReader:
+    def __init__(self, abi, web3, address,method_class=ReaderMethod):
+        if abi:
+            self.abi = abi
+            self._functions = filter_by_type('function', self.abi)
+            for func in self._functions:
+                _concise_method = method_class(
+                    ContractFunction.factory(
+                        func['name'],
+                        web3=web3,
+                        contract_abi=self.abi,
+                        address=address,
+                        function_identifier=func['name'])
+                )
+
+                setattr(self, func['name'], _concise_method)
+
+
 class ContractFunctions:
     """Class containing contract function objects
     """
@@ -241,6 +281,7 @@ class Contract:
     clone_bin = None
 
     functions = None
+    reader = None
 
     #: Instance of :class:`ContractEvents` presenting available Event ABIs
     events = None
@@ -271,6 +312,7 @@ class Contract:
             raise TypeError("The address argument is required to instantiate a contract.")
 
         self.functions = ContractFunctions(self.abi, self.web3, self.address)
+        self.reader = ContractReader(self.abi, self.web3, self.address) # TODO: Change to caller
         self.events = ContractEvents(self.abi, self.web3, self.address)
         self.fallback = Contract.get_fallback_function(self.abi, self.web3, self.address)
 
@@ -293,6 +335,7 @@ class Contract:
             normalizers=normalizers,
         )
         contract.functions = ContractFunctions(contract.abi, contract.web3)
+        contract.reader = ContractReader(contract.abi, contract.web3, contract.address) # TODO: Change to caller
         contract.events = ContractEvents(contract.abi, contract.web3)
         contract.fallback = Contract.get_fallback_function(contract.abi, contract.web3)
 
