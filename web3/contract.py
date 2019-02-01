f71ab2bc638d9637f31c9d165bae23439be12824
kclowes <kclowes@users.noreply.github.com>
2019-02-01 14:20:49 -0700

    Add elif and space in error message

--- web3/contract.py
+++ web3/contract.py
@@ -1509,7 +1509,7 @@ class ContractCaller:
             raise NoABIFound(
                 "There is no ABI found for this contract.",
             )
-        if not self._functions or len(self._functions) == 0:
+        elif not self._functions or len(self._functions) == 0:
             raise NoABIFunctionsFound(
                 "The ABI for this contract contains no function definitions. ",
                 "Are you sure you provided the correct contract ABI?"
@@ -1518,7 +1518,7 @@ class ContractCaller:
             functions_available = ', '.join([fn['name'] for fn in self._functions])
             raise MismatchedABI(
                 "The function '{}' was not found in this contract's ABI. ".format(function_name),
-                "Here is a list of all of the function names found: {}".format(functions_available),
+                "Here is a list of all of the function names found: {}. ".format(functions_available),
                 "Did you mean to call one of those functions?"
             )
         else:
