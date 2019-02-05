abf65ce8fbadd11a24726177e0051ae3977c1d49
Bryant Eisenbach <fubuloubu@gmail.com>
2019-02-05 15:45:14 -0500

    (bug) Need Geth POA middleware to use Rinkeby

--- web3/auto/infura/rinkeby.py
+++ web3/auto/infura/rinkeby.py
@@ -3,6 +3,8 @@ from web3.providers.auto import (
     load_provider_from_uri,
 )
 
+from web3.middleware import geth_poa_middleware
+
 from .endpoints import (
     INFURA_RINKEBY_DOMAIN,
     build_infura_url,
@@ -11,3 +13,4 @@ from .endpoints import (
 _infura_url = build_infura_url(INFURA_RINKEBY_DOMAIN)
 
 w3 = Web3(load_provider_from_uri(_infura_url))
+w3.middleware_stack.inject(geth_poa_middleware, layer=0)
