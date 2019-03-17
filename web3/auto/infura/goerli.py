c04b58429191a629fe1347bef3b8d56f27eaa108
Bryant Eisenbach <fubuloubu@gmail.com>
2019-03-16 22:49:21 -0400

    feat: Added goerli endpoint
    fixes: #1229

---
+++ web3/auto/infura/goerli.py
@@ -0,0 +1,17 @@
+from web3 import Web3
+from web3.middleware import (
+    geth_poa_middleware,
+)
+from web3.providers.auto import (
+    load_provider_from_uri,
+)
+
+from .endpoints import (
+    INFURA_GOERLI_DOMAIN,
+    build_infura_url,
+)
+
+_infura_url = build_infura_url(INFURA_GOERLI_DOMAIN)
+
+w3 = Web3(load_provider_from_uri(_infura_url))
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
