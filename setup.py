78ffc01a8d6149f27090d6bf2eee62f3b9f83184
Keri <kclowes@users.noreply.github.com>
2019-03-20 15:46:56 -0600

    Add error if no Infura key is found

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
         "eth-hash[pycryptodome]>=0.2.0,<1.0.0",
         "eth-typing>=2.0.0,<3.0.0",
         "eth-utils>=1.3.0,<2.0.0",
-        "ethpm>=0.1.4a12,<1.0.0",
+        "ethpm>=0.1.4a13,<1.0.0",
         "hexbytes>=0.1.0,<1.0.0",
         "lru-dict>=1.1.6,<2.0.0",
         "requests>=2.16.0,<3.0.0",
--- web3/auto/infura/endpoints.py
+++ web3/auto/infura/endpoints.py
@@ -1,8 +1,11 @@
-import logging
 import os
 
 from eth_utils import (
     ValidationError,
+)
+
+from web3.exceptions import (
+    InfuraKeyNotFound,
 )
 
 INFURA_MAINNET_DOMAIN = 'mainnet.infura.io'
@@ -19,10 +22,9 @@ def load_api_key():
     key = os.environ.get('WEB3_INFURA_PROJECT_ID',
                          os.environ.get('WEB3_INFURA_API_KEY', ''))
     if key == '':
-        logging.getLogger('web3.auto.infura').warning(
-            "No Infura Project ID found. Add environment variable WEB3_INFURA_PROJECT_ID to "
-            "ensure continued API access after March 27th. "
-            "New keys are available at https://infura.io/register"
+        raise InfuraKeyNotFound(
+            "No Infura Project ID found. Please ensure "
+            "that the environment variable WEB3_INFURA_PROJECT_ID is set."
         )
     return key
 
@@ -31,13 +33,9 @@ def build_infura_url(domain):
     scheme = os.environ.get('WEB3_INFURA_SCHEME', WEBSOCKET_SCHEME)
     key = load_api_key()
 
-    if key and scheme == WEBSOCKET_SCHEME:
+    if scheme == WEBSOCKET_SCHEME:
         return "%s://%s/ws/v3/%s" % (scheme, domain, key)
-    elif key and scheme == HTTP_SCHEME:
-        return "%s://%s/v3/%s" % (scheme, domain, key)
-    elif scheme == WEBSOCKET_SCHEME:
-        return "%s://%s/ws/" % (scheme, domain)
     elif scheme == HTTP_SCHEME:
-        return "%s://%s/%s" % (scheme, domain, key)
+        return "%s://%s/v3/%s" % (scheme, domain, key)
     else:
         raise ValidationError("Cannot connect to Infura with scheme %r" % scheme)
