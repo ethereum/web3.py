9483b16ad8ae01aa356ba154fc83db7f6c044b7d
Keri <kclowes@users.noreply.github.com>
2019-03-25 16:10:43 -0600

    User can use their secret key to make Infura calls

--- web3/auto/infura/rinkeby.py
+++ web3/auto/infura/rinkeby.py
@@ -8,10 +8,12 @@ from web3.providers.auto import (
 
 from .endpoints import (
     INFURA_RINKEBY_DOMAIN,
+    build_http_headers,
     build_infura_url,
 )
 
+_headers = build_http_headers()
 _infura_url = build_infura_url(INFURA_RINKEBY_DOMAIN)
 
-w3 = Web3(load_provider_from_uri(_infura_url))
+w3 = Web3(load_provider_from_uri(_infura_url, _headers))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
