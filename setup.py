5eb891170629d71ac7452b33599db87406a2a3c1
Keri <kclowes@users.noreply.github.com>
2019-04-15 15:37:56 -0600

    Bump version: 5.0.0-alpha.9 → 5.0.0-alpha.10

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ extras_require['dev'] = (
 setup(
     name='web3',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='5.0.0-alpha.9',
+    version='5.0.0-alpha.10',
     description="""Web3.py""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
