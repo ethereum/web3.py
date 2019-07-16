e529586d5789657abdac2c98bf72e6987232dee0
Nick Gheorghita <nickgheorghita@gmail.com>
2019-07-16 10:59:54 -0500

    Include ethpm assets in sdist via manifest.in

--- ethpm/__init__.py
+++ ethpm/__init__.py
@@ -4,7 +4,6 @@ from pathlib import Path
 ETHPM_DIR = Path(__file__).parent
 ASSETS_DIR = ETHPM_DIR / "assets"
 SPEC_DIR: Path = ASSETS_DIR / "spec"
-ETHPM_SPEC_DIR = ETHPM_DIR.parent / "ethpm-spec"
 
 from .package import Package  # noqa: F401
 from .backends.registry import RegistryURI  # noqa: F401
