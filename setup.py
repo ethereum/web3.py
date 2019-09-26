d7532174a9e6238073d7531679b5197f6fc5f907
Keri <kclowes@users.noreply.github.com>
2019-09-26 12:23:57 -0600

    Bump version: 5.1.0 → 5.2.0

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ extras_require['dev'] = (
 setup(
     name='web3',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='5.1.0',
+    version='5.2.0',
     description="""Web3.py""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
