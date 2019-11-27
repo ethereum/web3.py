f0aba55bbc5c940bb200f2b5abfbd8fb6c702d2c
Nick Gheorghita <nickgheorghita@gmail.com>
2019-11-27 11:13:17 +0100

    Type hint web3._utils.empty

--- web3/_utils/empty.py
+++ web3/_utils/empty.py
@@ -1,8 +1,13 @@
+from web3._utils.compat import (
+    Literal,
+)
+
+
 class Empty:
-    def __bool__(self):
+    def __bool__(self) -> Literal[False]:
         return False
 
-    def __nonzero__(self):
+    def __nonzero__(self) -> Literal[False]:
         return False
 
 
