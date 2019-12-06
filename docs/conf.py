5ad7f8a41df64422b29578ce496b99e03cc5e2c7
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2019-12-06 11:02:20 +0100

    Add Matomo Tracking

--- docs/conf.py
+++ docs/conf.py
@@ -149,7 +149,10 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
+
+def setup(app):
+    app.add_js_file("js/matomo.js")
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
