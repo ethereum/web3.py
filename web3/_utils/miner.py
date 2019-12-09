ea2f3b851842fed0f0cc8e2b381c2b0778154515
Nick Gheorghita <nickgheorghita@gmail.com>
2019-12-09 13:02:53 +0100

    Add types to methods in personal & miner apis

--- web3/_utils/miner.py
+++ web3/_utils/miner.py
@@ -1,3 +1,11 @@
+from typing import (
+    Callable,
+)
+
+from eth_typing import (
+    ChecksumAddress,
+)
+
 from web3._utils.rpc_abi import (
     RPC,
 )
@@ -5,50 +13,54 @@ from web3.method import (
     Method,
     default_root_munger,
 )
+from web3.types import (
+    BlockNumber,
+    Wei,
+)
 
-makeDag = Method(
+makeDag: Method[Callable[[BlockNumber], bool]] = Method(
     RPC.miner_makeDag,
     mungers=[default_root_munger],
 )
 
 
-setExtra = Method(
+setExtra: Method[Callable[[str], bool]] = Method(
     RPC.miner_setExtra,
     mungers=[default_root_munger],
 )
 
 
-setEtherbase = Method(
+setEtherbase: Method[Callable[[ChecksumAddress], bool]] = Method(
     RPC.miner_setEtherbase,
     mungers=[default_root_munger],
 )
 
 
-setGasPrice = Method(
+setGasPrice: Method[Callable[[Wei], bool]] = Method(
     RPC.miner_setGasPrice,
     mungers=[default_root_munger],
 )
 
 
-start = Method(
+start: Method[Callable[[int], bool]] = Method(
     RPC.miner_start,
     mungers=[default_root_munger],
 )
 
 
-stop = Method(
+stop: Method[Callable[[], bool]] = Method(
     RPC.miner_stop,
     mungers=None,
 )
 
 
-startAutoDag = Method(
+startAutoDag: Method[Callable[[], bool]] = Method(
     RPC.miner_startAutoDag,
     mungers=None,
 )
 
 
-stopAutoDag = Method(
+stopAutoDag: Method[Callable[[], bool]] = Method(
     RPC.miner_stopAutoDag,
     mungers=None,
 )
--- web3/_utils/personal.py
+++ web3/_utils/personal.py
@@ -1,3 +1,20 @@
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+)
+
+from eth_typing import (
+    ChecksumAddress,
+    HexStr,
+)
+from hexbytes import (
+    HexBytes,
+)
+
 from web3._utils.rpc_abi import (
     RPC,
 )
@@ -5,56 +22,69 @@ from web3.method import (
     Method,
     default_root_munger,
 )
+from web3.types import (
+    TxParams,
+)
 
-importRawKey = Method(
+if TYPE_CHECKING:
+    from typing_extensions import (  # noqa: F401
+        Protocol,
+    )
+
+importRawKey: Method[Callable[[str, str], ChecksumAddress]] = Method(
     RPC.personal_importRawKey,
     mungers=[default_root_munger],
 )
 
 
-newAccount = Method(
+newAccount: Method[Callable[[str], ChecksumAddress]] = Method(
     RPC.personal_newAccount,
     mungers=[default_root_munger],
 )
 
 
-listAccounts = Method(
+listAccounts: Method[Callable[[], List[ChecksumAddress]]] = Method(
     RPC.personal_listAccounts,
     mungers=None,
 )
 
 
-sendTransaction = Method(
+sendTransaction: Method[Callable[[TxParams, str], HexBytes]] = Method(
     RPC.personal_sendTransaction,
     mungers=[default_root_munger],
 )
 
 
-lockAccount = Method(
+lockAccount: Method[Callable[[ChecksumAddress], bool]] = Method(
     RPC.personal_lockAccount,
     mungers=[default_root_munger],
 )
 
 
-unlockAccount = Method(
+class UnlockAccountWrapper("Protocol"):
+    def __call__(self, account: ChecksumAddress, passphrase: str, duration: int=None) -> bool:
+        pass
+
+
+unlockAccount: Method[UnlockAccountWrapper] = Method(
     RPC.personal_unlockAccount,
     mungers=[default_root_munger],
 )
 
 
-sign = Method(
+sign: Method[Callable[[str, ChecksumAddress, Optional[str]], HexStr]] = Method(
     RPC.personal_sign,
     mungers=[default_root_munger],
 )
 
 
-signTypedData = Method(
+signTypedData: Method[Callable[[Dict[str, Any], ChecksumAddress, str], HexStr]] = Method(
     RPC.personal_signTypedData,
     mungers=[default_root_munger],
 )
 
 
-ecRecover = Method(
+ecRecover: Method[Callable[[str, HexStr], ChecksumAddress]] = Method(
     RPC.personal_ecRecover,
     mungers=[default_root_munger],
 )
