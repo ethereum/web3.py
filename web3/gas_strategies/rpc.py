0d550f3b14033da6d099d0630e2bd11807b193ac
Paul Lange <palango@gmx.de>
2020-03-24 12:27:51 +0100

    Fix return type of `rpc_gas_price_strategy`

--- web3/gas_strategies/rpc.py
+++ web3/gas_strategies/rpc.py
@@ -4,10 +4,11 @@ from web3._utils.rpc_abi import (
 )
 from web3.types import (
     TxParams,
+    Wei,
 )
 
 
-def rpc_gas_price_strategy(web3: Web3, transaction_params: TxParams=None) -> int:
+def rpc_gas_price_strategy(web3: Web3, transaction_params: TxParams=None) -> Wei:
     """
     A simple gas price strategy deriving it's value from the eth_gasPrice JSON-RPC call.
     """
