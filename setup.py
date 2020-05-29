9e52566999550491a8dfcfd45533579b4c6b3007
Marc Garreau <marcdgarreau@gmail.com>
2020-05-29 13:25:53 -0600

    temp test against current eth-tester branch

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ extras_require = {
         # TODO - remove this eth-keys pinning once eth-tester >0.4.0-beta.1 is released
         # and py-evm v0.3.0-alpha.15 is released
         "eth-keys>=0.2.1,<0.3",
-        "eth-tester[py-evm]==v0.2.0-beta.2",
+        "eth-tester[py-evm] @ git+https://github.com/ethereum/eth-tester@276eaeb3739ec7acaf410a86838fe8f2b487d172",
         "py-geth>=2.2.0,<3",
     ],
     'linter': [
