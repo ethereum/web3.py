4aac4d2a525c047274bfd6902e315727541ae3f5
Sahil sen <41318044+Sahilsen@users.noreply.github.com>
2020-07-17 01:17:38 +0530

    Create __init__.py

---
+++ web3/auto/QuikNode/__init__.py
@@ -0,0 +1,13 @@
+from web3 import Web3
+from web3.providers.auto import (
+  load_provider_from_uri,
+)
+
+from .endpoints import(
+  build_QuikNode_url,
+)
+
+#By default, connect to mainnet endpoint
+_QuikNode_url = build_QuikNode_url("mainnet")
+
+w3 = Web3(load_provider_from_uri(_QuikNode_url))
