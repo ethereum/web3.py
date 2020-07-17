a008f9df7a9a1458bb13f8c7df608d4be6e11ea4
Marc Garreau <marcdgarreau@gmail.com>
2020-07-16 20:07:16 -0600

    adopt trinity's newsfragment validation (#1690)
    * adopt trinity's newsfragment validation
    
    * add newsfragment

--- newsfragments/validate_files.py
+++ newsfragments/validate_files.py
@@ -5,13 +5,14 @@
 
 import os
 import pathlib
+import sys
 
 ALLOWED_EXTENSIONS = {
-    '.feature.rst',
     '.bugfix.rst',
     '.doc.rst',
-    '.removal.rst',
+    '.feature.rst',
     '.misc.rst',
+    '.removal.rst',
 }
 
 ALLOWED_FILES = {
@@ -21,11 +22,20 @@ ALLOWED_FILES = {
 
 THIS_DIR = pathlib.Path(__file__).parent
 
+num_args = len(sys.argv) - 1
+assert num_args in {0, 1}
+if num_args == 1:
+    assert sys.argv[1] in ('is-empty', )
+
 for fragment_file in THIS_DIR.iterdir():
 
     if fragment_file.name in ALLOWED_FILES:
         continue
-
-    full_extension = "".join(fragment_file.suffixes)
-    if full_extension not in ALLOWED_EXTENSIONS:
+    elif num_args == 0:
+        full_extension = "".join(fragment_file.suffixes)
+        if full_extension not in ALLOWED_EXTENSIONS:
+            raise Exception(f"Unexpected file: {fragment_file}")
+    elif sys.argv[1] == 'is-empty':
         raise Exception(f"Unexpected file: {fragment_file}")
+    else:
+        raise RuntimeError("Strange: arguments {sys.argv} were validated, but not found")
