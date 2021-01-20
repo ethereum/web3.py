b7baeca0cd92dda123f34931a695e30556b4b270
Jason Carver <ut96caarrs@snkmail.com>
2021-01-19 16:15:22 -0800

    Pin sphinx-rtd-theme to <v1
    Fixes https://github.com/ethereum/ethereum-python-project-template/issues/36

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras_require = {
     ],
     'doc': [
         "Sphinx>=1.6.5,<2",
-        "sphinx_rtd_theme>=0.1.9",
+        "sphinx_rtd_theme>=0.1.9,<1",
         "towncrier>=19.2.0, <20",
     ],
     'dev': [
