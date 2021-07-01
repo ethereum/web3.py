7f16fde3af8c27375b2eacbfcd84798cfded8d3e
Felipe Selmo <fselmo2@gmail.com>
2021-06-30 18:10:11 -0600

    Fixed some tests and turned some tests back on.
    Some of our tests had 'gasPrice' set too low. This would cause those transactions to sit in the tx pool - often interfering with other tests that were either looking for a mined transaction or looking to be included in the pending block. This commit attempts to fix that and turn some tests that were previously failing back on.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -15,30 +15,6 @@ from web3._utils.module_testing.revert_contract import (
 )
 
 
-def pytest_collection_modifyitems(items):
-    """
-    Certain tests have issues running after most of our test suite has run. If we run
-    these tests in the beginning, we can ensure that the client isn't overloaded with
-    context from our other tests and therefore reduce the noise for these more sensitive
-    tests. We can somewhat control when tests are run by overriding this pytest hook and
-    customizing the test order. You may find it helpful to move a conflicting test to
-    the beginning of the test suite or configure the test order in some other way here.
-    """
-    test_names_to_append_to_start = [
-        'test_eth_get_logs_with_logs',
-        'test_eth_call_old_contract_state',
-    ]
-    for index, item in enumerate(items):
-        # We run two versions of some tests that end with [<lambda>] or [address_conversion_func1].
-        # This step cleans up the name depending on whether or not they exist as two separate
-        # tests or just one without the bracketed "[]" suffix.
-        test_name = item.name if "[" not in item.name else item.name[:item.name.find("[")]
-
-        if test_name in test_names_to_append_to_start:
-            test_item = items.pop(index)
-            items.insert(0, test_item)
-
-
 @pytest.fixture(scope="module")
 def math_contract_factory(web3):
     contract_factory = web3.eth.contract(abi=MATH_ABI, bytecode=MATH_BYTECODE)
