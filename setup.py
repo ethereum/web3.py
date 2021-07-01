f13d78a6388fb4727f7ee7d11d4bd12674c67dc2
kclowes <kclowes@users.noreply.github.com>
2021-07-01 15:03:13 -0600

    Bump version: 5.20.0 → 5.20.1

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ with open('./README.md') as readme:
 setup(
     name='web3',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='5.20.0',
+    version='5.20.1',
     description="""Web3.py""",
     long_description_content_type='text/markdown',
     long_description=long_description,
