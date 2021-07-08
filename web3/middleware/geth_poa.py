6757ef18c4dee38b1d702ec63d317aea5a71268f
kclowes <kclowes@users.noreply.github.com>
2021-07-08 09:33:19 -0600

    Don't apply geth poa field remapping if result is None

--- web3/middleware/geth_poa.py
+++ web3/middleware/geth_poa.py
@@ -1,20 +1,25 @@
 from eth_utils.curried import (
+    apply_formatter_if,
     apply_formatters_to_dict,
     apply_key_map,
+    is_null,
 )
 from eth_utils.toolz import (
+    complement,
     compose,
 )
 from hexbytes import (
     HexBytes,
 )
 
+from web3._utils.rpc_abi import (
+    RPC,
+)
 from web3.middleware.formatting import (
     construct_formatting_middleware,
 )
-from web3.types import (
-    RPCEndpoint,
-)
+
+is_not_null = complement(is_null)
 
 remap_geth_poa_fields = apply_key_map({
     'extraData': 'proofOfAuthorityData',
@@ -28,7 +33,7 @@ geth_poa_cleanup = compose(pythonic_geth_poa, remap_geth_poa_fields)
 
 geth_poa_middleware = construct_formatting_middleware(
     result_formatters={
-        RPCEndpoint("eth_getBlockByHash"): geth_poa_cleanup,
-        RPCEndpoint("eth_getBlockByNumber"): geth_poa_cleanup,
+        RPC.eth_getBlockByHash: apply_formatter_if(is_not_null, geth_poa_cleanup),
+        RPC.eth_getBlockByNumber: apply_formatter_if(is_not_null, geth_poa_cleanup),
     },
 )
