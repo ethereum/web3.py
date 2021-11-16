79e05c596a2fd488d69e8e6784604f01eccb05ce
Dario <medariox@users.noreply.github.com>
2021-11-16 10:45:21 +0100

    Add chain_id setter

--- tests/core/eth-module/test_eth_properties.py
+++ tests/core/eth-module/test_eth_properties.py
@@ -2,7 +2,8 @@ import pytest
 
 
 def test_eth_protocol_version(w3):
-    assert w3.eth.protocol_version == '63'
+    with pytest.warns(DeprecationWarning):
+        assert w3.eth.protocol_version == '63'
 
 
 def test_eth_protocolVersion(w3):
@@ -17,3 +18,13 @@ def test_eth_chain_id(w3):
 def test_eth_chainId(w3):
     with pytest.warns(DeprecationWarning):
         assert w3.eth.chainId == 61
+
+
+def test_set_chain_id(w3):
+    assert w3.eth.chain_id == 61
+
+    w3.eth.chain_id = 72
+    assert w3.eth.chain_id == 72
+
+    w3.eth.chain_id = None
+    assert w3.eth.chain_id == 61
