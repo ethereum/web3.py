2020b5519e4307c1e13b581424f431d9c9405293
AlwaysData <dbfreem2@yahoo.com>
2021-12-02 16:03:44 -0500

    Fix for HypothesisDeprecationWarning in CircleCI #1729 (#2228)
    * Added conftest.py to test/core/utilities and moved web3 fixture to it
    Moved fixtures into test_contract_topic_filter.py
    Both of these were to fix HypothesisDeprecationWarning while running test
    
    Co-authored-by: kclowes <kclowes@users.noreply.github.com>

---
+++ tests/core/utilities/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from web3.main import (
+    Web3,
+)
+from web3.providers.eth_tester import (
+    EthereumTesterProvider,
+)
+
+
+@pytest.fixture(scope="module")
+def web3():
+    provider = EthereumTesterProvider()
+    return Web3(provider)
