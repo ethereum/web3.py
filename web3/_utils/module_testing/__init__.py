6a90a26ea12e5a789834c9cd6a7ae6d302648f88
AlwaysData <dbfreem2@yahoo.com>
2022-01-07 14:49:47 -0500

    Add async geth txpool (#2273)

--- web3/_utils/module_testing/__init__.py
+++ web3/_utils/module_testing/__init__.py
@@ -5,6 +5,10 @@ from .eth_module import (  # noqa: F401
 from .go_ethereum_admin_module import (  # noqa: F401
     GoEthereumAdminModuleTest,
 )
+from .go_ethereum_txpool_module import (  # noqa: F401
+    GoEthereumAsyncTxPoolModuleTest,
+    GoEthereumTxPoolModuleTest
+)
 from .net_module import (  # noqa: F401
     AsyncNetModuleTest,
     NetModuleTest,
