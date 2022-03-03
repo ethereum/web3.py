a58882b77ad0d1596367ab252ac1635741a9bca1
kclowes <kclowes@users.noreply.github.com>
2022-03-03 13:35:46 -0700

    Add async chain_id setter

--- tests/core/eth-module/test_eth_properties.py
+++ tests/core/eth-module/test_eth_properties.py
@@ -1,5 +1,23 @@
 import pytest
 
+from web3 import Web3
+from web3.eth import (
+    AsyncEth,
+)
+from web3.providers.eth_tester.main import (
+    AsyncEthereumTesterProvider,
+)
+
+
+@pytest.fixture
+def async_w3():
+    return Web3(
+        AsyncEthereumTesterProvider(),
+        middlewares=[],
+        modules={
+            'eth': (AsyncEth,),
+        })
+
 
 def test_eth_protocol_version(web3):
     with pytest.warns(DeprecationWarning):
@@ -28,3 +46,14 @@ def test_set_chain_id(web3):
 
     web3.eth.chain_id = None
     assert web3.eth.chain_id == 61
+
+
+@pytest.mark.asyncio
+async def test_async_set_chain_id(async_w3):
+    assert await async_w3.eth.chain_id == 61
+
+    async_w3.eth.chain_id = 72
+    assert await async_w3.eth.chain_id == 72
+
+    async_w3.eth.chain_id = None
+    assert await async_w3.eth.chain_id == 61
