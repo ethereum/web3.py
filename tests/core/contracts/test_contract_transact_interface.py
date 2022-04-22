f7540b338d14e2232fa819d306e74a3baa5dfe7d
pacrob <paul@pacrob.com>
2022-04-22 19:54:21 +0200

    test async contract transact interface

--- tests/core/contracts/test_contract_transact_interface.py
+++ tests/core/contracts/test_contract_transact_interface.py
@@ -442,152 +442,176 @@ async def test_async_transact_when_default_account_is_set(
     assert txn_after['from'] == async_w3.eth.default_account
 
 
-def test_transacting_with_contract_with_string_argument(w3, string_contract, transact, call):
+@pytest.mark.asyncio
+async def test_async_transacting_with_contract_with_string_argument(
+        async_w3,
+        async_string_contract,
+        async_transact,
+        async_call):
     # eth_abi will pass as raw bytes, no encoding
     # unless we encode ourselves
-    txn_hash = transact(contract=string_contract,
-                        contract_function='setValue',
-                        func_args=["ÄLÄMÖLÖ".encode('utf8')])
-    txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
+    txn_hash = await async_transact(contract=async_string_contract,
+                                    contract_function='setValue',
+                                    func_args=["ÄLÄMÖLÖ".encode('utf8')])
+    txn_receipt = await async_w3.eth.wait_for_transaction_receipt(txn_hash)
     assert txn_receipt is not None
 
-    final_value = call(contract=string_contract,
-                       contract_function='getValue')
+    final_value = await async_call(contract=async_string_contract,
+                                   contract_function='getValue')
 
     assert final_value == "ÄLÄMÖLÖ"
 
 
-def test_transacting_with_contract_with_bytes32_array_argument(w3,
-                                                               arrays_contract,
-                                                               transact,
-                                                               call):
+@pytest.mark.asyncio
+async def test_async_transacting_with_contract_with_bytes32_array_argument(
+        async_w3,
+        async_arrays_contract,
+        async_transact,
+        async_call):
     # new_bytes32_array = [keccak('1'), keccak('2'), keccak('3')]
     new_bytes32_array = [
         b'\xc8\x9e\xfd\xaaT\xc0\xf2\x0cz\xdfa(\x82\xdf\tP\xf5\xa9Qc~\x03\x07\xcd\xcbLg/)\x8b\x8b\xc6',  # noqa: E501
         b'\xad|[\xef\x02x\x16\xa8\x00\xda\x176DO\xb5\x8a\x80~\xf4\xc9`;xHg?~:h\xeb\x14\xa5',
         b"*\x80\xe1\xef\x1dxB\xf2\x7f.k\xe0\x97+\xb7\x08\xb9\xa15\xc3\x88`\xdb\xe7<'\xc3Hl4\xf4\xde",  # noqa: E501
     ]
-    txn_hash = transact(contract=arrays_contract,
-                        contract_function="setBytes32Value",
-                        func_args=[new_bytes32_array])
-    txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
+    txn_hash = await async_transact(contract=async_arrays_contract,
+                                    contract_function="setBytes32Value",
+                                    func_args=[new_bytes32_array])
+    txn_receipt = await async_w3.eth.wait_for_transaction_receipt(txn_hash)
     assert txn_receipt is not None
 
-    final_value = call(contract=arrays_contract,
-                       contract_function="getBytes32Value")
+    final_value = await async_call(contract=async_arrays_contract,
+                                   contract_function="getBytes32Value")
     assert final_value == new_bytes32_array
 
 
-def test_transacting_with_contract_with_byte_array_argument(w3, arrays_contract, transact, call):
+@pytest.mark.asyncio
+async def test_async_transacting_with_contract_with_byte_array_argument(
+        async_w3,
+        async_arrays_contract,
+        async_transact,
+        async_call):
     new_byte_array = [b'\x03', b'\x03', b'\x03', b'\x03', b'\x03', b'\x03']
-    txn_hash = transact(contract=arrays_contract,
-                        contract_function='setByteValue',
-                        func_args=[new_byte_array])
-    txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
+    txn_hash = await async_transact(contract=async_arrays_contract,
+                                    contract_function='setByteValue',
+                                    func_args=[new_byte_array])
+    txn_receipt = await async_w3.eth.wait_for_transaction_receipt(txn_hash)
     assert txn_receipt is not None
 
-    final_value = call(contract=arrays_contract,
-                       contract_function='getByteValue')
+    final_value = await async_call(contract=async_arrays_contract,
+                                   contract_function='getByteValue')
     assert final_value == new_byte_array
 
 
-def test_transacting_with_contract_respects_explicit_gas(w3,
-                                                         STRING_CONTRACT,
-                                                         skip_if_testrpc,
-                                                         wait_for_block,
-                                                         call,
-                                                         transact):
-    skip_if_testrpc(w3)
+@pytest.mark.asyncio
+async def test_async_transacting_with_contract_respects_explicit_gas(
+        async_w3,
+        STRING_CONTRACT,
+        async_skip_if_testrpc,
+        async_wait_for_block,
+        async_call,
+        async_transact):
+    await async_skip_if_testrpc(async_w3)
 
-    wait_for_block(w3)
+    await async_wait_for_block(async_w3)
 
-    StringContract = w3.eth.contract(**STRING_CONTRACT)
+    StringContract = async_w3.eth.contract(**STRING_CONTRACT)
 
     deploy_txn = StringContract.constructor("Caqalai").transact()
-    deploy_receipt = w3.eth.wait_for_transaction_receipt(deploy_txn, 30)
+    deploy_receipt = await async_w3.eth.wait_for_transaction_receipt(deploy_txn, 30)
     assert deploy_receipt is not None
     string_contract = StringContract(address=deploy_receipt['contractAddress'])
 
     # eth_abi will pass as raw bytes, no encoding
     # unless we encode ourselves
-    txn_hash = transact(contract=string_contract,
-                        contract_function='setValue',
-                        func_args=[to_bytes(text="ÄLÄMÖLÖ")],
-                        tx_kwargs={'gas': 200000})
-    txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash, 30)
+    txn_hash = await async_transact(contract=string_contract,
+                                    contract_function='setValue',
+                                    func_args=[to_bytes(text="ÄLÄMÖLÖ")],
+                                    tx_kwargs={'gas': 200000})
+    txn_receipt = await async_w3.eth.wait_for_transaction_receipt(txn_hash, 30)
     assert txn_receipt is not None
 
-    final_value = call(contract=string_contract,
-                       contract_function='getValue')
+    final_value = await async_call(contract=string_contract,
+                                   contract_function='getValue')
     assert to_bytes(text=final_value) == to_bytes(text="ÄLÄMÖLÖ")
 
-    txn = w3.eth.get_transaction(txn_hash)
+    txn = await async_w3.eth.get_transaction(txn_hash)
     assert txn['gas'] == 200000
 
 
-def test_auto_gas_computation_when_transacting(w3,
-                                               STRING_CONTRACT,
-                                               skip_if_testrpc,
-                                               wait_for_block,
-                                               call,
-                                               transact):
-    skip_if_testrpc(w3)
+@pytest.mark.asyncio
+async def test_async_auto_gas_computation_when_transacting(
+        async_w3,
+        STRING_CONTRACT,
+        async_skip_if_testrpc,
+        async_wait_for_block,
+        async_call,
+        async_transact):
+    await async_skip_if_testrpc(async_w3)
 
-    wait_for_block(w3)
+    await async_wait_for_block(async_w3)
 
-    StringContract = w3.eth.contract(**STRING_CONTRACT)
+    StringContract = async_w3.eth.contract(**STRING_CONTRACT)
 
-    deploy_txn = StringContract.constructor("Caqalai").transact()
-    deploy_receipt = w3.eth.wait_for_transaction_receipt(deploy_txn, 30)
+    deploy_txn = await StringContract.constructor("Caqalai").transact()
+    deploy_receipt = await async_w3.eth.wait_for_transaction_receipt(deploy_txn, 30)
     assert deploy_receipt is not None
     string_contract = StringContract(address=deploy_receipt['contractAddress'])
 
-    gas_estimate = string_contract.functions.setValue(to_bytes(text="ÄLÄMÖLÖ")).estimate_gas()
+    gas_estimate = await string_contract.functions.setValue(to_bytes(text="ÄLÄMÖLÖ")).estimate_gas()
 
     # eth_abi will pass as raw bytes, no encoding
     # unless we encode ourselves
-    txn_hash = transact(contract=string_contract,
-                        contract_function="setValue",
-                        func_args=[to_bytes(text="ÄLÄMÖLÖ")])
-    txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash, 30)
+    txn_hash = await async_transact(contract=string_contract,
+                                    contract_function="setValue",
+                                    func_args=[to_bytes(text="ÄLÄMÖLÖ")])
+    txn_receipt = await async_w3.eth.wait_for_transaction_receipt(txn_hash, 30)
     assert txn_receipt is not None
 
-    final_value = call(contract=string_contract,
-                       contract_function='getValue')
+    final_value = await async_call(contract=string_contract,
+                                   contract_function='getValue')
     assert to_bytes(text=final_value) == to_bytes(text="ÄLÄMÖLÖ")
 
-    txn = w3.eth.get_transaction(txn_hash)
+    txn = await async_w3.eth.get_transaction(txn_hash)
     assert txn['gas'] == gas_estimate + 100000
 
 
-def test_fallback_transacting_with_contract(w3, fallback_function_contract, call):
-    initial_value = call(contract=fallback_function_contract,
-                         contract_function='getData')
-    txn_hash = fallback_function_contract.fallback.transact()
-    txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
+@pytest.mark.asyncio
+async def test_async_fallback_transacting_with_contract(
+        async_w3,
+        async_fallback_function_contract,
+        async_call):
+    initial_value = await async_call(contract=async_fallback_function_contract,
+                                     contract_function='getData')
+    txn_hash = await async_fallback_function_contract.fallback.transact()
+    txn_receipt = await async_w3.eth.wait_for_transaction_receipt(txn_hash)
     assert txn_receipt is not None
 
-    final_value = call(contract=fallback_function_contract,
-                       contract_function='getData')
+    final_value = await async_call(contract=async_fallback_function_contract,
+                                   contract_function='getData')
 
     assert final_value - initial_value == 1
 
 
-def test_receive_function(receive_function_contract, call):
-    initial_value = call(contract=receive_function_contract,
-                         contract_function='getText')
+@pytest.mark.asyncio
+async def test_async_receive_function(async_receive_function_contract, async_call):
+    initial_value = await async_call(contract=async_receive_function_contract,
+                                     contract_function='getText')
     assert initial_value == ''
-    receive_function_contract.receive.transact()
-    final_value = call(contract=receive_function_contract,
-                       contract_function='getText')
+    await async_receive_function_contract.receive.transact()
+    final_value = await async_call(contract=async_receive_function_contract,
+                                   contract_function='getText')
     assert final_value == 'receive'
 
 
-def test_receive_contract_with_fallback_function(receive_function_contract, call):
-    initial_value = call(contract=receive_function_contract,
-                         contract_function='getText')
+@pytest.mark.asyncio
+async def test_async_receive_contract_with_fallback_function(
+        async_receive_function_contract,
+        async_call):
+    initial_value = await async_call(contract=async_receive_function_contract,
+                                     contract_function='getText')
     assert initial_value == ''
-    receive_function_contract.fallback.transact()
-    final_value = call(contract=receive_function_contract,
-                       contract_function='getText')
+    await async_receive_function_contract.fallback.transact()
+    final_value = await async_call(contract=async_receive_function_contract,
+                                   contract_function='getText')
     assert final_value == 'receive'
