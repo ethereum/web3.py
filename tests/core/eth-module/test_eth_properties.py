3e8f07c14f10f97071de2f93180e0be07a575ffa
kclowes <kclowes@users.noreply.github.com>
2022-04-27 13:31:13 -0600

    Add sync chain_id to simple cache (#2426)
    * Cache eth.chain_id in simple_cache_middleware (#2425)
    
    * Add caching for sync chain_id
    
    * Remove async chainId setter
    
    Will add async chainId to the cache once the simple_cache_middleware is available on async
    
    * Add docs
    
    * Add newsfragment for simple_cache_middleware, take out old, confusing newsfragment

--- tests/core/eth-module/test_eth_properties.py
+++ tests/core/eth-module/test_eth_properties.py
@@ -1,23 +1,5 @@
 import pytest
 
-from web3 import Web3
-from web3.eth import (
-    AsyncEth,
-)
-from web3.providers.eth_tester.main import (
-    AsyncEthereumTesterProvider,
-)
-
-
-@pytest.fixture
-def async_w3():
-    return Web3(
-        AsyncEthereumTesterProvider(),
-        middlewares=[],
-        modules={
-            'eth': (AsyncEth,),
-        })
-
 
 def test_eth_protocol_version(w3):
     with pytest.warns(DeprecationWarning):
@@ -36,24 +18,3 @@ def test_eth_chain_id(w3):
 def test_eth_chainId(w3):
     with pytest.warns(DeprecationWarning):
         assert w3.eth.chainId == 61
-
-
-def test_set_chain_id(w3):
-    assert w3.eth.chain_id == 61
-
-    w3.eth.chain_id = 72
-    assert w3.eth.chain_id == 72
-
-    w3.eth.chain_id = None
-    assert w3.eth.chain_id == 61
-
-
-@pytest.mark.asyncio
-async def test_async_set_chain_id(async_w3):
-    assert await async_w3.eth.chain_id == 61
-
-    async_w3.eth.chain_id = 72
-    assert await async_w3.eth.chain_id == 72
-
-    async_w3.eth.chain_id = None
-    assert await async_w3.eth.chain_id == 61
