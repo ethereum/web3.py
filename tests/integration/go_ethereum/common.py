ea74b7a815d9eaafd5e26d5a12a942b2f499a617
Felipe Selmo <fselmo2@gmail.com>
2022-05-02 12:18:40 -0600

    Add ``xfail`` ``strict=False`` to eth estimate_gas tests.
    - Add more flaky tests to the list that have timeout issues.

--- tests/integration/go_ethereum/common.py
+++ tests/integration/go_ethereum/common.py
@@ -1,4 +1,11 @@
 import pytest
+from typing import (
+    TYPE_CHECKING,
+)
+
+from eth_typing import (
+    ChecksumAddress,
+)
 
 from web3._utils.module_testing import (  # noqa: F401
     AsyncEthModuleTest,
@@ -12,6 +19,14 @@ from web3._utils.module_testing import (  # noqa: F401
     VersionModuleTest,
     Web3ModuleTest,
 )
+from web3.types import (
+    BlockData,
+)
+
+if TYPE_CHECKING:
+    from web3 import (  # noqa: F401
+        Web3,
+    )
 
 
 class GoEthereumTest(Web3ModuleTest):
@@ -39,6 +54,46 @@ class GoEthereumEthModuleTest(EthModuleTest):
     @pytest.mark.xfail(reason='eth_protocolVersion was removed in Geth 1.10.0')
     def test_eth_protocolVersion(self, web3):
         super().test_eth_protocolVersion(web3)
+
+    @pytest.mark.xfail(reason='Inconsistently creating timeout issues.', strict=False)
+    def test_eth_estimate_gas(
+        self, web3: "Web3", unlocked_account_dual_type: ChecksumAddress
+    ) -> None:
+        super().test_eth_estimate_gas(web3, unlocked_account_dual_type)
+
+    @pytest.mark.xfail(reason='Inconsistently creating timeout issues.', strict=False)
+    def test_eth_estimateGas_deprecated(
+        self, web3: "Web3", unlocked_account_dual_type: ChecksumAddress
+    ) -> None:
+        super().test_eth_estimateGas_deprecated(web3, unlocked_account_dual_type)
+
+    @pytest.mark.xfail(reason='Inconsistently creating timeout issues.', strict=False)
+    def test_eth_estimate_gas_with_block(
+        self, web3: "Web3", unlocked_account_dual_type: ChecksumAddress
+    ) -> None:
+        super().test_eth_estimate_gas_with_block(web3, unlocked_account_dual_type)
+
+    @pytest.mark.xfail(reason='Inconsistently creating timeout issues.', strict=False)
+    def test_eth_get_transaction_receipt_unmined(
+        self, web3: "Web3", unlocked_account_dual_type: ChecksumAddress
+    ) -> None:
+        super().test_eth_get_transaction_receipt_unmined(web3, unlocked_account_dual_type)
+
+    @pytest.mark.xfail(reason='Inconsistently creating timeout issues.', strict=False)
+    def test_eth_wait_for_transaction_receipt_unmined(
+        self, web3: "Web3", unlocked_account_dual_type: ChecksumAddress
+    ) -> None:
+        super().test_eth_wait_for_transaction_receipt_unmined(web3, unlocked_account_dual_type)
+
+    @pytest.mark.xfail(reason='Inconsistently creating timeout issues.', strict=False)
+    def test_eth_get_raw_transaction_by_block(
+        self, web3: "Web3",
+        unlocked_account_dual_type: ChecksumAddress,
+        block_with_txn: BlockData,
+    ) -> None:
+        super().test_eth_get_raw_transaction_by_block(
+            web3, unlocked_account_dual_type, block_with_txn
+        )
 
 
 class GoEthereumVersionModuleTest(VersionModuleTest):
