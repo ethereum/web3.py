8077ddf4741eeee3ade19d93f35f2b1e498ced20
DB <dbfreem2@yahoo.com>
2022-05-04 22:17:46 -0400

    merge conflict

--- tests/core/eth-module/test_eth_properties.py
+++ tests/core/eth-module/test_eth_properties.py
@@ -1,26 +1,2 @@
-import pytest
-
-
 def test_eth_chain_id(w3):
     assert w3.eth.chain_id == 61
-
-
-def test_set_chain_id(w3):
-    assert w3.eth.chain_id == 61
-
-    w3.eth.chain_id = 72
-    assert w3.eth.chain_id == 72
-
-    w3.eth.chain_id = None
-    assert w3.eth.chain_id == 61
-
-
-@pytest.mark.asyncio
-async def test_async_set_chain_id(async_w3):
-    assert await async_w3.eth.chain_id == 61
-
-    async_w3.eth.chain_id = 72
-    assert await async_w3.eth.chain_id == 72
-
-    async_w3.eth.chain_id = None
-    assert await async_w3.eth.chain_id == 61
