649faad47cc9def2491fef32bd372e9ef06a4a2b
Don Freeman <dbfreem2@yahoo.com>
2022-05-17 11:35:42 -0400

    removed verison check in __init__ (#2478)
    * removed verison check in __init__

--- web3/__init__.py
+++ web3/__init__.py
@@ -1,5 +1,3 @@
-import sys
-import warnings
 
 import pkg_resources
 
@@ -24,18 +22,6 @@ from web3.providers.async_rpc import (  # noqa: E402
 from web3.providers.websocket import (  # noqa: E402
     WebsocketProvider,
 )
-
-if (3, 5) <= sys.version_info < (3, 6):
-    warnings.warn(
-        "Support for Python 3.5 will be removed in web3.py v5",
-        category=DeprecationWarning,
-        stacklevel=2)
-
-if sys.version_info < (3, 5):
-    raise EnvironmentError(
-        "Python 3.5 or above is required. "
-        "Note that support for Python 3.5 will be removed in web3.py v5")
-
 
 __version__ = pkg_resources.get_distribution("web3").version
 
