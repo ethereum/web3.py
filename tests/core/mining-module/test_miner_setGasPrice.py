93ca8df8b16f2b3ff616a908f33116df2a4944c6
kclowes <kclowes@users.noreply.github.com>
2022-06-08 12:09:26 -0600

    Remove setGasPrice test

--- tests/core/mining-module/test_miner_setGasPrice.py
+++ tests/core/mining-module/test_miner_setGasPrice.py
@@ -27,23 +27,3 @@ def test_miner_set_gas_price(web3_empty, wait_for_block):
 
     after_gas_price = web3.eth.gas_price
     assert after_gas_price < initial_gas_price
-
-
-@flaky(max_runs=3)
-def test_miner_setGasPrice(web3_empty, wait_for_block):
-    web3 = web3_empty
-
-    initial_gas_price = web3.eth.gas_price
-    assert web3.eth.gas_price > 1000
-
-    # sanity check
-
-    with pytest.warns(DeprecationWarning):
-        web3.geth.miner.setGasPrice(initial_gas_price // 2)
-
-    with Timeout(60) as timeout:
-        while web3.eth.gas_price == initial_gas_price:
-            timeout.sleep(random.random())
-
-    after_gas_price = web3.eth.gas_price
-    assert after_gas_price < initial_gas_price
