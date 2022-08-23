eb1a696730a42b147650a4f183f6a387a61a76a3
Marc Garreau <marcdgarreau@gmail.com>
2022-08-23 13:52:17 -0600

    update beacon api to 2.3.0

--- web3/beacon/__init__.py
+++ web3/beacon/__init__.py
@@ -1,7 +1,1 @@
-import warnings
 from .main import Beacon  # noqa: F401
-
-warnings.warn(
-    "Beacon node APIs are experimental and may not be "
-    "implemented consistently by all clients.",
-)
