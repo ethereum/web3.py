f9859205a423cd002497e35515f57d6ea23fbbb3
kclowes <kclowes@users.noreply.github.com>
2022-09-07 14:43:03 -0600

    Remove references to the deprecated Kovan testnet

--- web3/auto/infura/endpoints.py
+++ web3/auto/infura/endpoints.py
@@ -20,7 +20,6 @@ INFURA_MAINNET_DOMAIN = "mainnet.infura.io"
 INFURA_ROPSTEN_DOMAIN = "ropsten.infura.io"
 INFURA_GOERLI_DOMAIN = "goerli.infura.io"
 INFURA_RINKEBY_DOMAIN = "rinkeby.infura.io"
-INFURA_KOVAN_DOMAIN = "kovan.infura.io"
 
 WEBSOCKET_SCHEME = "wss"
 HTTP_SCHEME = "https"
