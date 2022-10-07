1afb632e7548a3beab6e3ceefa71b9243173ef31
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-10-07 13:11:40 +0200

    Adding PyChain 2022 banner

--- docs/conf.py
+++ docs/conf.py
@@ -151,7 +151,10 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_css_files = ["css/toggle.css"]
+html_css_files = [
+    "css/toggle.css",
+    "css/banner.css",
+]
 
 html_js_files = ["js/matomo.js", "js/toggle.js"]
 
