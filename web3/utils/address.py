28e0acb0efd258d7ef54f17faff49341936c1596
Tino Martínez Molina <tino@dekalabs.com>
2022-12-17 18:33:05 +0100

    PR suggestions

--- web3/utils/address.py
+++ web3/utils/address.py
@@ -4,11 +4,10 @@ from eth_typing import (
 )
 from eth_utils import (
     keccak,
+    to_bytes,
     to_checksum_address,
 )
-from rlp import (
-    encode,
-)
+import rlp
 
 from web3.types import (
     HexStr,
@@ -20,9 +19,7 @@ def get_create_address(sender: HexAddress, nonce: Nonce) -> ChecksumAddress:
     """
     Determine the resulting `CREATE` opcode contract address for a sender and a nonce.
     """
-    bytes_sender = bytes.fromhex(sender[2:])
-
-    contract_address = keccak(encode([bytes_sender, nonce])).hex()[-40:]
+    contract_address = keccak(rlp.encode([to_bytes(hexstr=sender), nonce])).hex()[-40:]
     return to_checksum_address(contract_address)
 
 
@@ -33,12 +30,10 @@ def get_create2_address(
     Determine the resulting `CREATE2` opcode contract address for a sender, salt and
     bytecode.
     """
-    bytes_prefix = bytes.fromhex("0xFF"[2:])
-    bytes_sender = bytes.fromhex(sender[2:])
-    bytes_salt = bytes.fromhex(salt[2:])
-    bytes_init_code = bytes.fromhex(init_code[2:])
-
     contract_address = keccak(
-        bytes_prefix + bytes_sender + bytes_salt + keccak(bytes_init_code)
+        b"\xff"
+        + to_bytes(hexstr=sender)
+        + to_bytes(hexstr=salt)
+        + keccak(to_bytes(hexstr=init_code))
     ).hex()[-40:]
     return to_checksum_address(contract_address)
