8ffab14c68e390535a23e7e4c704cdfc9d54be93
kclowes <kclowes@users.noreply.github.com>
2023-01-19 15:36:21 -0700

    CamelCase params

--- web3/contract/async_contract.py
+++ web3/contract/async_contract.py
@@ -342,8 +342,8 @@ class AsyncContractEvent(BaseContractEvent):
     async def get_logs(
         self,
         argument_filters: Optional[Dict[str, Any]] = None,
-        from_block: Optional[BlockIdentifier] = None,
-        to_block: Optional[BlockIdentifier] = None,
+        fromBlock: Optional[BlockIdentifier] = None,
+        toBlock: Optional[BlockIdentifier] = None,
         block_hash: Optional[HexBytes] = None,
     ) -> Awaitable[Iterable[EventData]]:
         """Get events for this contract instance using eth_getLogs API.
@@ -365,7 +365,7 @@ class AsyncContractEvent(BaseContractEvent):
             from = max(mycontract.web3.eth.block_number - 10, 1)
             to = mycontract.web3.eth.block_number
 
-            events = mycontract.events.Transfer.getLogs(from_block=from, to_block=to)
+            events = mycontract.events.Transfer.getLogs(fromBlock=from, toBlock=to)
 
             for e in events:
                 print(e["args"]["from"],
@@ -394,17 +394,17 @@ class AsyncContractEvent(BaseContractEvent):
         See also: :func:`web3.middleware.filter.local_filter_middleware`.
 
         :param argument_filters:
-        :param from_block: block number or "latest", defaults to "latest"
-        :param to_block: block number or "latest". Defaults to "latest"
+        :param fromBlock: block number or "latest", defaults to "latest"
+        :param toBlock: block number or "latest". Defaults to "latest"
         :param blockHash: block hash. blockHash cannot be set at the
-          same time as from_block or to_block
+          same time as fromBlock or toBlock
         :yield: Tuple of :class:`AttributeDict` instances
         """
         abi = self._get_event_abi()
         # Call JSON-RPC API
         logs = await self.w3.eth.get_logs(
             self._get_event_filter_params(
-                abi, argument_filters, from_block, to_block, block_hash  # type: ignore
+                abi, argument_filters, fromBlock, toBlock, block_hash  # type: ignore
             )
         )
 
@@ -418,8 +418,8 @@ class AsyncContractEvent(BaseContractEvent):
         self,
         *,  # PEP 3102
         argument_filters: Optional[Dict[str, Any]] = None,
-        from_block: Optional[BlockIdentifier] = None,
-        to_block: BlockIdentifier = "latest",
+        fromBlock: Optional[BlockIdentifier] = None,
+        toBlock: BlockIdentifier = "latest",
         address: Optional[ChecksumAddress] = None,
         topics: Optional[Sequence[Any]] = None,
     ) -> AsyncLogFilter:
@@ -429,8 +429,8 @@ class AsyncContractEvent(BaseContractEvent):
         filter_builder = AsyncEventFilterBuilder(self._get_event_abi(), self.w3.codec)
         self._set_up_filter_builder(
             argument_filters,
-            from_block,
-            to_block,
+            fromBlock,
+            toBlock,
             address,
             topics,
             filter_builder,
