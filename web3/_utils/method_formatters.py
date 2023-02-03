d2712eddf884f413140e89f060fd582095d0d92e
joseluu <jose.luu@gmail.com>
2023-02-03 12:29:05 +0100

    make ContractCustomError derive from ContractLogicError for compatibility with existing exception handlers, sending to a wrong selector leads to an error with no data, this should be a ContractLogicError as before

--- web3/_utils/method_formatters.py
+++ web3/_utils/method_formatters.py
@@ -758,6 +758,10 @@ def raise_contract_logic_error_on_revert(response: RPCResponse) -> RPCResponse:
         raise OffchainLookup(offchain_lookup_payload)
 
     # Geth case:
+    if len(data) >= 10 and not data[:10] == "0x08c379a0":  # custom error processing
+        # raising along with the data value to allow processing in user code
+        raise ContractCustomError(data)
+
     if "message" in response["error"] and response["error"].get("code", "") == 3:
         raise ContractLogicError(response["error"]["message"])
 
