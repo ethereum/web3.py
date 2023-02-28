91e530e06906448590928ef8e3d563e91f49c0d6
e3243eric <e0525eric@gmail.com>
2023-03-01 03:58:37 +0800

    Fix web3 lint errors

--- web3/_utils/events.py
+++ web3/_utils/events.py
@@ -140,7 +140,7 @@ def construct_event_topic_set(
         for arg, arg_options in zipped_abi_and_args
     ]
 
-    topics = list(normalize_topic_list([event_topic] + encoded_args))  # type: ignore
+    topics = list(normalize_topic_list([event_topic] + encoded_args))
     return topics
 
 
@@ -394,12 +394,12 @@ class BaseEventFilterBuilder:
     def ordered_args(self) -> Tuple[Any, ...]:
         return tuple(map(self.args.__getitem__, self._ordered_arg_names))
 
-    @property  # type: ignore
+    @property
     @to_tuple
     def indexed_args(self) -> Tuple[Any, ...]:
         return tuple(filter(is_indexed, self.ordered_args))
 
-    @property  # type: ignore
+    @property
     @to_tuple
     def data_args(self) -> Tuple[Any, ...]:
         return tuple(filter(is_not_indexed, self.ordered_args))
@@ -432,7 +432,7 @@ class EventFilterBuilder(BaseEventFilterBuilder):
         if not isinstance(w3, web3.Web3):
             raise ValueError(f"Invalid web3 argument: got: {w3!r}")
 
-        for arg in AttributeDict.values(self.args):
+        for arg in self.args.values():
             arg._immutable = True
         self._immutable = True
 
