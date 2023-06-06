c99135587614c4f95275ea27dd3227f45ac23c73
Marc Garreau <3621728+wolovim@users.noreply.github.com>
2023-06-05 21:17:23 -0400

    docs: update matomo script (#2978)

--- docs/_static/js/matomo.js
+++ docs/_static/js/matomo.js
@@ -1,11 +1,12 @@
-var _paq = window._paq || [];
+var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(["setExcludedQueryParams", ["code","gist"]]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="https://matomo.ethereum.org/piwik/";
+  var u="https://ethereumfoundation.matomo.cloud/";
   _paq.push(['setTrackerUrl', u+'matomo.php']);
   _paq.push(['setSiteId', '18']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  g.async=true; g.src='//cdn.matomo.cloud/ethereumfoundation.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
 })();
