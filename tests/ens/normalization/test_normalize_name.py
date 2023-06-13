abe37729ffce9f603496210d7b03852b9e293983
fselmo <fselmo2@gmail.com>
2023-06-13 17:01:43 -0600

    Continue ENSIP-15 implementation and add tests

--- tests/ens/normalization/test_normalize_name.py
+++ tests/ens/normalization/test_normalize_name.py
@@ -1,21 +1,41 @@
 import json
 import os
 
+import pytest
+
+from ens import InvalidName
 from ens.utils import normalize_name
 
 
-NORMALIZATION_TESTS = os.path.join(
+NORMALIZATION_TESTS_PATH = os.path.join(
     os.path.dirname(__file__), "normalization_tests.json"
 )
+with open(NORMALIZATION_TESTS_PATH) as f:
+    normalization_tests = json.load(f)
+
+POSITIVE_TEST_CASES = [test for test in normalization_tests if "error" not in test]
+NEGATIVE_TEST_CASES = [test for test in normalization_tests if "error" in test]
 
 
-def test_normalize_name_from_spec_tests():
-    with open(NORMALIZATION_TESTS) as f:
-        normalization_tests = json.load(f)
+@pytest.mark.parametrize(
+    "positive_test_case",
+    POSITIVE_TEST_CASES,
+    ids=lambda t: t["name"],
+)
+def test_normalize_name_positive_test_cases(positive_test_case):
+    name = positive_test_case["name"]
 
-    for test_vector in normalization_tests:
-        name = test_vector["name"]
+    expected = positive_test_case.get("norm", positive_test_case.get("name"))
+    assert normalize_name(name) == expected
 
-        if "error" not in test_vector:
-            expected = test_vector.get("norm", test_vector.get("name"))
-            assert normalize_name(name) == expected
+
+@pytest.mark.parametrize(
+    "negative_test_case",
+    NEGATIVE_TEST_CASES,
+    ids=lambda t: t["name"],
+)
+def test_normalize_name_negative_test_cases(negative_test_case):
+    name = negative_test_case["name"]
+
+    with pytest.raises(InvalidName):
+        normalize_name(name)
