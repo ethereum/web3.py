f5267b4fdd5e2d5d6e1b1afd3d58f6bb7fec6d74
pacrob <pacrob@protonmail.com>
2023-06-26 16:14:09 -0600

    drop typing-extensions upper pin and note py-version for types

--- web3/_utils/compat/__init__.py
+++ web3/_utils/compat/__init__.py
@@ -5,4 +5,12 @@
 # because `typing_extensions` will do it for us and either import from `typing`
 # or use the back-ported version of the type.
 
-from typing_extensions import Literal, Protocol, TypedDict, Self  # noqa: F401
+# Once web3 supports >= the noted python version, the type may be directly
+# imported from `typing`
+
+from typing_extensions import (  # noqa: F401
+    Literal,  # py38
+    Protocol,  # py38
+    TypedDict,  # py38
+    Self,  # py311
+)
