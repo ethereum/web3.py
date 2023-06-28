71af710e217db44cb1e105ff0929c0e7051739a1
fselmo <fselmo2@gmail.com>
2023-06-28 13:36:51 -0600

    Create a storage contract and compile it

---
+++ web3/_utils/contract_sources/StorageContract.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.8.20;
+
+contract StorageContract {
+        int public slot0 = 0x0;
+        int public slot1 = 0x1;
+        int public slot2 = 0x2;
+        string public slot3 = "three";
+        bytes public slot4 = bytes("four");
+}
