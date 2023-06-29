d1e2b9c8c16573f457fa11fa420e669136c91acd
fselmo <fselmo2@gmail.com>
2023-06-29 10:39:04 -0600

    bump eth-tester version to ``v0.9.0-b.2``

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import (
 
 extras_require = {
     "tester": [
-        "eth-tester[py-evm]==v0.9.0-b.1",
+        "eth-tester[py-evm]==v0.9.0-b.2",
         "py-geth>=3.11.0",
     ],
     "linter": [
