fe0fa4f87d25e5586572088ffef7df624a5eb400
Stuart Reed <stuart.reed@ethereum.org>
2023-07-06 14:27:47 -0600

    Use AsyncHTTPProvider for logger

--- web3/providers/__init__.py
+++ web3/providers/__init__.py
@@ -1,6 +1,9 @@
 from .async_base import (  # noqa: F401
     AsyncBaseProvider,
 )
+from .async_rpc import (  # noqa: F401
+    AsyncHTTPProvider,
+)
 from .base import (  # noqa: F401
     BaseProvider,
     JSONBaseProvider,
