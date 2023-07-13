e5cdaa2ec207eca6bb524526f797e611856e9b13
Sebastian Dine <sebastiandine91@gmail.com>
2023-07-13 08:34:23 +0000

    formatting

--- web3/utils/formatters.py
+++ web3/utils/formatters.py
@@ -9,20 +9,20 @@ def parseUnits(value: str, decimals: int) -> int:
     """
     Parse formatted decimal string `value` to an `int` considering
     the provided `decimal` places.
-    This can be used to convert a formatted string to a numeric 
+    This can be used to convert a formatted string to a numeric
     value in the smallest denomination of the asset, e.g.
     ```
     parseUnits("1.5", 18) -> 1500000000000000000
-    ``` 
+    ```
     """
     value = _trim_trailing_zeros(value)
     decimals_pos = value.find('.')
     if decimals_pos == -1:
         return int(value+"0"*decimals)
     else:
-        return int(value[0:decimals_pos]\
-            +value[decimals_pos+1:]\
-            +"0"*(decimals - len(value[decimals_pos+1:])))
+        return int(value[0:decimals_pos]
+        + value[decimals_pos+1:]
+        + "0"*(decimals - len(value[decimals_pos+1:])))
 
 
 def formatUnits(value: int, decimals: int) -> str:
@@ -34,15 +34,15 @@ def formatUnits(value: int, decimals: int) -> str:
     string in ETH.
     ```
     formatUnits(1500000000000000000, 18) -> "1.5"
-    ``` 
+    ```
     """
     value = str(value)
     if len(value) > decimals:
-        return _trim_trailing_zeros(value[0:len(value)-decimals]\
-            +"."+value[len(value)-decimals:])
+        return _trim_trailing_zeros(value[0:len(value)-decimals]
+        + "." + value[len(value)-decimals:])
     else:
-        return _trim_trailing_zeros("0."+"0"*(decimals-len(value))+value)
-    
+        return _trim_trailing_zeros("0." + "0"*(decimals-len(value))+value)
+
 
 def formatEther(value: int) -> str:
     """
