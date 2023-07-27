517ed249235e65ff62e4233bfa13872e0849d853
fselmo <fselmo2@gmail.com>
2023-07-27 13:37:44 -0600

    Add a test suite for AsyncWeb3 w/ ws v2 as an ``AsyncIterator``
    - Separate the goethereum_ws_v2 tests into a directory with ``AsyncGenerator`` / "async with w3..." style tests and ``AsyncIterator`` / "async for w3..." style tests

---
+++ tests/integration/go_ethereum/test_goethereum_ws_v2/conftest.py
@@ -0,0 +1,49 @@
+import pytest
+
+from tests.integration.common import (
+    COINBASE,
+)
+from tests.utils import (
+    get_open_port,
+)
+
+
+@pytest.fixture(scope="module")
+def ws_port():
+    return get_open_port()
+
+
+@pytest.fixture(scope="module")
+def endpoint_uri(ws_port):
+    return f"ws://localhost:{ws_port}"
+
+
+def _geth_command_arguments(ws_port, base_geth_command_arguments, geth_version):
+    yield from base_geth_command_arguments
+    if geth_version.major == 1:
+        yield from (
+            "--miner.etherbase",
+            COINBASE[2:],
+            "--ws",
+            "--ws.port",
+            ws_port,
+            "--ws.api",
+            "admin,eth,net,web3,personal,miner",
+            "--ws.origins",
+            "*",
+            "--ipcdisable",
+            "--allow-insecure-unlock",
+        )
+        if geth_version.minor not in [10, 11]:
+            raise AssertionError("Unsupported Geth version")
+    else:
+        raise AssertionError("Unsupported Geth version")
+
+
+@pytest.fixture(scope="module")
+def geth_command_arguments(
+    geth_binary, get_geth_version, datadir, ws_port, base_geth_command_arguments
+):
+    return _geth_command_arguments(
+        ws_port, base_geth_command_arguments, get_geth_version
+    )
