e0599691d899bcb5513f85188d077eda9ae917c0
fselmo <fselmo2@gmail.com>
2023-10-16 12:10:32 -0600

    Add integration tests for async await pattern for wsV2

--- tests/integration/go_ethereum/test_goethereum_ws_v2/test_async_await_w3.py
+++ tests/integration/go_ethereum/test_goethereum_ws_v2/test_async_await_w3.py
@@ -29,9 +29,8 @@ from ..utils import (
 async def async_w3(geth_process, endpoint_uri):
     await wait_for_aiohttp(endpoint_uri)
 
-    # async context manager pattern
-    async with AsyncWeb3.persistent_websocket(WebsocketProviderV2(endpoint_uri)) as w3:
-        yield w3
+    # await the persistent connection itself
+    return await AsyncWeb3.persistent_websocket(WebsocketProviderV2(endpoint_uri))
 
 
 class TestGoEthereumAsyncAdminModuleTest(GoEthereumAsyncAdminModuleTest):
