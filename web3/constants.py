f14936aa4fb4165cdb8904d8c17b0c6e1b6748b3
Uxío <Uxio0@users.noreply.github.com>
2023-11-07 23:27:53 +0100

    Add types for constants (#3138)

--- web3/constants.py
+++ web3/constants.py
@@ -1,7 +1,14 @@
+from eth_typing import (
+    ChecksumAddress,
+    HexAddress,
+    HexStr,
+)
+
 # Constants as Strings
-ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
-MAX_INT = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-HASH_ZERO = "0x0000000000000000000000000000000000000000000000000000000000000000"
+ADDRESS_ZERO = HexAddress(HexStr("0x0000000000000000000000000000000000000000"))
+CHECKSUM_ADDRESSS_ZERO = ChecksumAddress(ADDRESS_ZERO)
+MAX_INT = HexStr("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
+HASH_ZERO = HexStr("0x0000000000000000000000000000000000000000000000000000000000000000")
 
 # Constants as Int
 WEI_PER_ETHER = 1000000000000000000
