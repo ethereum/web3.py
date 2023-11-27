62f9eda0ef73429b799f8e9f60fba027bbb45cb7
Stuart Reed <stuart.reed@ethereum.org>
2023-11-27 12:24:18 -0700

    Bump version: 6.11.3 → 6.11.4

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ with open("./README.md") as readme:
 setup(
     name="web3",
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version="6.11.3",
+    version="6.11.4",
     description="""web3.py""",
     long_description_content_type="text/markdown",
     long_description=long_description,
