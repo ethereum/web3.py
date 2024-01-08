e342fe762491aa61a4f21be0683fe507f0ee89ba
pacrob <pacrob@protonmail.com>
2024-01-08 16:18:53 -0700

    add version to import test

--- tests/core/test_import.py
+++
@@ -1,2 +0,0 @@
-def test_import():
-    import <MODULE_NAME>  # noqa: F401
---
+++ tests/core/test_import_and_version.py
@@ -0,0 +1,4 @@
+def test_import():
+    import <MODULE_NAME>
+    version = <MODULE_NAME>.__version__
+    assert isinstance(version, str)
