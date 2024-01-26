b853f5ccdf0fc45b2168ffaffc52adccbe098ac1
pacrob <5199899+pacrob@users.noreply.github.com>
2024-01-26 11:14:40 -0700

    add py312 support

--- setup.py
+++ setup.py
@@ -67,5 +67,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
 )
