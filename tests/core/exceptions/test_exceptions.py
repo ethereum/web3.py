ec21bc93c2c5afcbe51eccf2762d97348426b014
Stuart Reed <stuart.reed@ethereum.org>
2024-03-12 11:00:49 -0600

    Tests for raising Web3Exception

---
+++ tests/core/exceptions/test_exceptions.py
@@ -0,0 +1,30 @@
+import pytest
+
+from web3.exceptions import (
+    Web3Exception,
+)
+
+
+def test_web3exception_with_user_message():
+    with pytest.raises(Web3Exception) as exception:
+        raise Web3Exception(user_message="This failed!")
+    assert exception.type is Web3Exception
+    assert exception.value.user_message == "This failed!"
+
+
+def test_web3exception_with_kwargs():
+    with pytest.raises(Web3Exception) as exception:
+        raise Web3Exception(data={"message": "Unable to fulfill your request."})
+    assert exception.type is Web3Exception
+    assert exception.value.user_message is None
+    assert (
+        exception.value.kwargs["data"]["message"] == "Unable to fulfill your request."
+    )
+
+
+def test_web3exception_with_args():
+    with pytest.raises(Web3Exception) as exception:
+        raise Web3Exception("failed")
+    assert exception.type is Web3Exception
+    assert exception.value.user_message is None
+    assert exception.value.args[0] == "failed"
