744c75fff6af861585345763a49221591acd8467
fselmo <fselmo2@gmail.com>
2024-03-28 13:54:13 -0600

    Bump version: 7.0.0-beta.2 → 7.0.0-beta.3

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ with open("./README.md") as readme:
 setup(
     name="web3",
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version="7.0.0-beta.2",
+    version="7.0.0-beta.3",
     description="""web3.py""",
     long_description_content_type="text/markdown",
     long_description=long_description,
