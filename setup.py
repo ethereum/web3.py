ac76840b9b9514a1540f3c67df268a463bfc54b1
Stuart Reed <stuart.reed@ethereum.org>
2024-04-16 13:09:20 -0600

    Revert "add upperpin for eth-typing for removal of ethpm types"
    This reverts commit 748fa8cfc71a7e2e02b01449d5cb812800053645.

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
         "eth-abi>=4.0.0",
         "eth-account>=0.8.0,<0.13",
         "eth-hash[pycryptodome]>=0.5.1",
-        "eth-typing>=3.0.0,<4.2.0",
+        "eth-typing>=3.0.0",
         "eth-utils>=2.1.0",
         "hexbytes>=0.1.0,<0.4.0",
         "jsonschema>=4.0.0",
