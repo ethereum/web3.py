4f79d802d4ef938d2bbf373811e77ee71e93b52f
Stuart Reed <stuart.reed@ethereum.org>
2024-05-01 09:14:56 -0600

    Import from eth_utils

--- web3/utils/__init__.py
+++ web3/utils/__init__.py
@@ -4,8 +4,21 @@ classify as breaking changes.
 """
 
 from .abi import (  # NOQA
+    event_abi_to_log_topic,
+    event_signature_to_log_topic,
+    function_abi_to_4byte_selector,
+    function_signature_to_4byte_selector,
     get_abi_input_names,
+    get_abi_input_types,
     get_abi_output_names,
+    get_abi_output_types,
+    get_all_event_abis,
+    get_all_function_abis,
+    get_event_abi,
+    get_event_log_topics,
+    get_function_abi,
+    get_function_info,
+    get_normalized_abi_arg_type,
 )
 from .address import get_create_address  # NOQA
 from .async_exception_handling import (  # NOQA
--- web3/utils/abi.py
+++ web3/utils/abi.py
@@ -8,6 +8,24 @@ from web3.types import (
     ABIFunction,
 )
 
+from eth_utils import (
+    event_abi_to_log_topic,
+    event_signature_to_log_topic,
+    function_abi_to_4byte_selector,
+    function_signature_to_4byte_selector,
+    get_abi_input_names,
+    get_abi_input_types,
+    get_abi_output_names,
+    get_abi_output_types,
+    get_all_event_abis,
+    get_all_function_abis,
+    get_event_abi,
+    get_event_log_topics,
+    get_function_abi,
+    get_function_info,
+    get_normalized_abi_arg_type,
+)
+
 
 def get_abi_input_names(abi: Union[ABIFunction, ABIEvent]) -> List[str]:
     if "inputs" not in abi and abi["type"] == "fallback":
