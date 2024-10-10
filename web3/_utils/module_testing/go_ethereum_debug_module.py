69fb0c51771d8d4ea3c880b085adebd370b8c59b
kclowes <kclowes@users.noreply.github.com>
2024-10-10 10:57:39 -0600

    Add prestate tracer, and a few tests

--- web3/_utils/module_testing/go_ethereum_debug_module.py
+++ web3/_utils/module_testing/go_ethereum_debug_module.py
@@ -12,20 +12,37 @@ from web3 import (
 
 class GoEthereumAsyncDebugModuleTest:
     @pytest.mark.asyncio
-    async def test_async_geth_debug_trace_transaction_calltracer(
+    async def test_async_geth_debug_trace_transaction_call_tracer(
+        self, async_w3: "AsyncWeb3", txn_hash_with_log: HexStr
+    ) -> None:
+        result = await async_w3.geth.debug.trace_transaction(txn_hash_with_log)
+        assert result.get("type") in ["CALL", "CREATE"]
+
+    @pytest.mark.asyncio
+    async def test_async_geth_debug_trace_transaction_prestate_tracer(
         self, async_w3: "AsyncWeb3", txn_hash_with_log: HexStr
     ) -> None:
         result = await async_w3.geth.debug.trace_transaction(
-            txn_hash_with_log, {"tracer": "callTracer"}
+            txn_hash_with_log,
+            {"tracer": "prestateTracer", "tracerConfig": {"diffMode": True}},
         )
-        assert result["type"] in ["CALL", "CREATE"]
+        assert "post" in dict(result).keys()
+        assert "pre" in dict(result).keys()
 
 
 class GoEthereumDebugModuleTest:
-    def test_geth_debug_trace_transaction_calltracer(
+    def test_geth_debug_trace_transaction_call_tracer(
+        self, w3: "Web3", txn_hash_with_log: HexStr
+    ) -> None:
+        result = w3.geth.debug.trace_transaction(txn_hash_with_log)
+        assert result.get("type") in ["CALL", "CREATE"]
+
+    def test_geth_debug_trace_transaction_prestate_tracer(
         self, w3: "Web3", txn_hash_with_log: HexStr
     ) -> None:
         result = w3.geth.debug.trace_transaction(
-            txn_hash_with_log, {"tracer": "callTracer"}
+            txn_hash_with_log,
+            {"tracer": "prestateTracer", "tracerConfig": {"diffMode": True}},
         )
-        assert result["type"] in ["CALL", "CREATE"]
+        assert "post" in dict(result).keys()
+        assert "pre" in dict(result).keys()
