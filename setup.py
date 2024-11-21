1280617249d31d4fb069bb2a33f10535e25a3dfb
pacrob <5199899+pacrob@users.noreply.github.com>
2024-11-21 15:04:31 -0700

    run mypy local

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ extras_require = {
         "build>=0.9.0",
         "bump_my_version>=0.19.0",
         "ipython",
+        "mypy==1.10.0",
         "pre-commit>=3.4.0",
         "tox>=4.0.0",
         "twine",
