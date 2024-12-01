134bf4f312d5ece3d591e176922e805d536208f8
box25 <simon@klein-homepage.de>
2024-12-01 16:44:09 +0200

    remove verbose RPC error logging

--- web3/manager.py
+++ web3/manager.py
@@ -256,8 +256,6 @@ def _validate_response(
 
         response = apply_error_formatters(error_formatters, response)
 
-        logger.error(web3_rpc_error.user_message)
-        logger.debug(f"RPC error response: {response}")
         raise web3_rpc_error
 
     elif "result" not in response and not is_subscription_response:
