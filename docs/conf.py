83887422fbc5c1088ccc0bfb6b8959f861018286
Paul Robinson <5199899+pacrob@users.noreply.github.com>
2024-12-02 12:13:04 -0700

    correct use of __file__

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 
 import os
 
-DIR = os.path.dirname("__file__")
+DIR = os.path.dirname(__file__)
 with open(os.path.join(DIR, "../setup.py"), "r") as f:
     for line in f:
         if "version=" in line:
