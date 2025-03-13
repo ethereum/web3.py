2944ba4974d75c07ca335c3a591d22e0cce1dc3d
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-03-13 17:40:37 +0000

    feat: build log messages lazily

--- web3/providers/persistent/persistent.py
+++ web3/providers/persistent/persistent.py
@@ -171,9 +171,7 @@ class PersistentConnectionProvider(AsyncJSONBaseProvider, ABC):
         while _connection_attempts != self._max_connection_retries:
             try:
                 _connection_attempts += 1
-                self.logger.info(
-                    f"Connecting to: {self.get_endpoint_uri_or_ipc_path()}"
-                )
+                self.logger.info("Connecting to: %s", self.get_endpoint_uri_or_ipc_path())
                 await self._provider_specific_connect()
                 self._message_listener_task = asyncio.create_task(
                     self._message_listener()
@@ -181,9 +179,7 @@ class PersistentConnectionProvider(AsyncJSONBaseProvider, ABC):
                 self._message_listener_task.add_done_callback(
                     self._message_listener_callback
                 )
-                self.logger.info(
-                    f"Successfully connected to: {self.get_endpoint_uri_or_ipc_path()}"
-                )
+                self.logger.info("Successfully connected to: %s", self.get_endpoint_uri_or_ipc_path())
                 break
             except (WebSocketException, OSError) as e:
                 if _connection_attempts == self._max_connection_retries:
@@ -192,8 +188,9 @@ class PersistentConnectionProvider(AsyncJSONBaseProvider, ABC):
                         f"Retries exceeded max of {self._max_connection_retries}."
                     ) from e
                 self.logger.info(
-                    f"Could not connect to: {self.get_endpoint_uri_or_ipc_path()}. "
-                    f"Retrying in {round(_backoff_time, 1)} seconds.",
+                    "Could not connect to: %s. Retrying in %s seconds.",
+                    self.get_endpoint_uri_or_ipc_path(),
+                    round(_backoff_time, 1),
                     exc_info=True,
                 )
                 await asyncio.sleep(_backoff_time)
@@ -213,9 +210,7 @@ class PersistentConnectionProvider(AsyncJSONBaseProvider, ABC):
 
         await self._provider_specific_disconnect()
         self._request_processor.clear_caches()
-        self.logger.info(
-            f"Successfully disconnected from: {self.get_endpoint_uri_or_ipc_path()}"
-        )
+        self.logger.info("Successfully disconnected from: %s", self.get_endpoint_uri_or_ipc_path())
 
     @async_handle_send_caching
     async def send_request(self, method: RPCEndpoint, params: Any) -> RPCRequest:
@@ -332,9 +327,9 @@ class PersistentConnectionProvider(AsyncJSONBaseProvider, ABC):
 
     async def _message_listener(self) -> None:
         self.logger.info(
-            f"{self.__class__.__qualname__} listener background task started. Storing "
+            "%s listener background task started. Storing "
             "all messages in appropriate request processor queues / caches to be "
-            "processed."
+            "processed.", self.__class__.__qualname__
         )
         while True:
             # the use of sleep(0) seems to be the most efficient way to yield control
@@ -358,8 +353,8 @@ class PersistentConnectionProvider(AsyncJSONBaseProvider, ABC):
                 self._raise_stray_errors_from_cache()
             except PersistentConnectionClosedOK as e:
                 self.logger.info(
-                    "Message listener background task has ended gracefully: "
-                    f"{e.user_message}"
+                    "Message listener background task has ended gracefully: %s",
+                    e.user_message,
                 )
                 # trigger a return to end the listener task and initiate the callback fn
                 return
@@ -377,8 +372,8 @@ class PersistentConnectionProvider(AsyncJSONBaseProvider, ABC):
         """
         self.logger.error(
             "Exception caught in listener, error logging and keeping "
-            "listener background task alive."
-            f"\n    error={e.__class__.__name__}: {e}"
+            "listener background task alive.\n    error=%s: %s",
+            e.__class__.__name__, e
         )
 
     def _handle_listener_task_exceptions(self) -> None:
@@ -410,9 +405,7 @@ class PersistentConnectionProvider(AsyncJSONBaseProvider, ABC):
                 self._handle_listener_task_exceptions()
 
                 if request_cache_key in self._request_processor._request_response_cache:
-                    self.logger.debug(
-                        f"Popping response for id {request_id} from cache."
-                    )
+                    self.logger.debug("Popping response for id %s from cache.", request_id)
                     popped_response = await self._request_processor.pop_raw_response(
                         cache_key=request_cache_key,
                     )
