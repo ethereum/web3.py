480a3e808f7988adee8d98db58bc2e981a94564e
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-05 14:06:57 -0400

    Update names.py

--- web3/middleware/names.py
+++ web3/middleware/names.py
@@ -24,7 +24,6 @@ from web3.types import (
 )
 
 from .._utils.abi import (
-    IteratorProxy,
     abi_data_tree,
     async_data_tree_map,
     strip_abi_type,
@@ -59,11 +58,11 @@ async def async_format_all_ens_names_to_address(
     data: Sequence[Any],
 ) -> Sequence[Any]:
     # provide a stepwise version of what the curried formatters do
-    abi_typed_params = abi_data_tree(abi_types_for_method, data)
+    abi_typed_params = list(abi_data_tree(abi_types_for_method, data))
     formatted_data_tree = await async_data_tree_map(
         async_web3,
         async_abi_ens_resolver,
-        IteratorProxy(abi_typed_params),
+        abi_typed_params,
     )
     formatted_params = recursive_map(strip_abi_type, formatted_data_tree)
     return formatted_params
