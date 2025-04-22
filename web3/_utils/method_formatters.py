41ef2caf026e3bbb3b57d40623060d6648a2f8e6
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-04-22 17:01:03 +0000

    chore: refactor out _apply_request_formatters

--- web3/_utils/method_formatters.py
+++ web3/_utils/method_formatters.py
@@ -1069,7 +1069,7 @@ def get_request_formatters(
         PYTHONIC_REQUEST_FORMATTERS,
     )
     formatters = combine_formatters(request_formatter_maps, method_name)
-    return compose(*formatters)
+    return compose(tuple, *formatters)
 
 
 def raise_block_not_found(params: Tuple[BlockIdentifier, bool]) -> NoReturn:
