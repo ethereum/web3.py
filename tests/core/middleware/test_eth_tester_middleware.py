bd53261560eb135964adee9c69493a59d7b21ef2
fselmo <fselmo2@gmail.com>
2025-05-19 20:19:33 -0600

    make core tests able to be run with pytest-xdist:
    - Remove sets from parametrization since they can be in random order.
      Either use sorted or change to tuples / lists.
    
    - run with ``-n auto`` and set ``--maxprocesses=15`` for core test runs.

--- tests/core/middleware/test_eth_tester_middleware.py
+++ tests/core/middleware/test_eth_tester_middleware.py
@@ -18,7 +18,7 @@ SAMPLE_ADDRESS_LIST = [
 SAMPLE_ADDRESS = "0x0000000000000000000000000000000000000004"
 
 
-@pytest.mark.parametrize("block_number", {0, "0x0", "earliest"})
+@pytest.mark.parametrize("block_number", (0, "0x0", "earliest"))
 def test_get_transaction_count_formatters(w3, block_number):
     tx_counts = w3.eth.get_transaction_count(w3.eth.accounts[-1], block_number)
     assert tx_counts == 0
