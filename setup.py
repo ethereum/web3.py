578e4ab4ccc6cf3d4cd77f4a8281ebb3a4b8a11d
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-11 16:51:42 -0400

    chore: add Python3.14 classifier in setup.py

--- setup.py
+++ setup.py
@@ -163,5 +163,6 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
 )
